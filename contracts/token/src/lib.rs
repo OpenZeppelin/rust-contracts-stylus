8b43549c2d88f0d39969f39caf3f77d25e947781
Alexander Gonzalez <alexfertel97@gmail.com>
2024-03-18 11:26:53 +0100

    feat(token): add a panic handler for no_std envs

--- contracts/token/src/lib.rs
+++ contracts/token/src/lib.rs
@@ -3,3 +3,9 @@ extern crate alloc;
 
 #[global_allocator]
 static ALLOC: mini_alloc::MiniAlloc = mini_alloc::MiniAlloc::INIT;
+
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
