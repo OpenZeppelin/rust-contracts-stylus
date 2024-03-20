8207d37b8345c6ffecfb7af5959f340cd19174fc
Alexander Gonzalez <alexfertel97@gmail.com>
2024-03-20 16:26:19 +0100

    feat(contracts): add a test-utils crate

---
+++ contracts/test-utils/src/lib.rs
@@ -0,0 +1,26 @@
+//! Unit-testing utilities for Stylus contracts.
+use std::sync::{Mutex, MutexGuard};
+
+pub use wavm_shims::*;
+
+/// A global static mutex.
+///
+/// We use this for scenarios where concurrent mutation of storage is wanted.
+/// For example, when a test harness is running, this ensures each test
+/// accesses storage in an non-overlapping manner.
+///
+/// See [`with_storage`].
+pub static STORAGE_MUTEX: Mutex<()> = Mutex::new(());
+
+/// Acquires access to storage.
+pub fn acquire_storage() -> MutexGuard<'static, ()> {
+    STORAGE_MUTEX.lock().unwrap()
+}
+
+/// Decorates a closure by running it with exclusive access to storage.
+pub fn with_storage<C: Default>(closure: impl FnOnce(&mut C)) {
+    let _lock = acquire_storage();
+    let mut contract = C::default();
+    closure(&mut contract);
+    reset_storage();
+}
