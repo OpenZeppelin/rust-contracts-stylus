b2d309ace99949e8c99fd11067f2e4f82a0e41f1
Alexander González <alexfertel97@gmail.com>
2024-05-06 14:25:47 +0100

    ref(merkle): rework API (#48)
    ### Motivation
    
    The current implementation of merkle proof verification is a port from
    the `openzeppelin-contracts` library that contains almost no
    modifications.
    
    This work is related to improving the API to make it more
    Rust-native and easier to integrate with, add a few optimizations,
    remove unnecessary dependencies, and add a corresponding example
    contract that uses it.
    
    It is a follow-up to #47 
    
    ### API update
    
    The main change behind the new API is for it to resemble the
    `core::hash` module, since that's the interface used in Rust's standard
    library. We can't use it as is, because of the signature of
    `core::hash::Hasher::finish`, which returns a `u64` (we need a
    customizable return type).
    
    ---
    
    - [x] Improve the API by introducing a `Verifier` struct -- taking name
    suggestions!
    - [x] [Optimization] Add inline hints where possible.
      - [x] Add inline hint to `hash_sorted_pair`.
      - [x] Add inline hint to `hash`.
      - [x] Add inline hint to `build_hasher`.
    - [x] [Optimization] Use `core::mem::swap` instead of `(a, b) = (b, a)`.
    - [x] Remove unnecessary dependency on `alloy_primitives`.
    - We use `tiny_keccak` instead now, so that the default `Hasher` used is
    `keccak256`.
    - [x] Remove the dev-dependency in `const-hex`.
    - We use `hex-literal` now because it allows for more readable tests.
    - [x] Polish API
      - [x] Use errors instead of panics.
      - [x] Try to simplify types and generics.
    - [x] Add MerkleProof contract example.
    - [x] Try to simplify docs, wording, tests...
    - [x] Add missing docs and tests.

--- lib/crypto/src/lib.rs
+++ lib/crypto/src/lib.rs
@@ -1,8 +1,12 @@
 #![doc = include_str!("../README.md")]
 #![warn(missing_docs, unreachable_pub, rust_2021_compatibility)]
 #![warn(clippy::all, clippy::pedantic)]
-#![cfg_attr(not(test), no_main, no_std)]
+#![cfg_attr(not(feature = "std"), no_std, no_main)]
 extern crate alloc;
 
-#[cfg(feature = "merkle")]
+pub mod hash;
+#[cfg(any(feature = "std", feature = "merkle"))]
 pub mod merkle;
+
+pub mod keccak;
+pub use keccak::KeccakBuilder;
