ae44f1756727c29b026eda1ff661ad965975b88b
Alisander Qoshqosh <qalisander@gmail.com>
2024-05-10 16:07:21 +0400

    add mint to erc721 example

--- examples/erc721/src/lib.rs
+++ examples/erc721/src/lib.rs
@@ -1,6 +1,8 @@
 #![cfg_attr(not(test), no_main, no_std)]
 extern crate alloc;
 
+use alloy_primitives::Address;
+use stylus_sdk::alloy_sol_types::private::U256;
 use contracts::erc721::ERC721;
 use stylus_sdk::prelude::{entrypoint, external, sol_storage};
 
@@ -14,4 +16,8 @@ sol_storage! {
 
 #[external]
 #[inherit(ERC721)]
-impl Token {}
+impl Token {
+    pub fn mint(&mut self, to: Address, token_id: U256) -> Result<(), contracts::erc721::Error> {
+        self.erc721._mint(to, token_id)
+    }
+}
