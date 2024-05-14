b785362d2e2f04ef23771ed047167335d22059d2
Alexander Gonzalez <alexfertel97@gmail.com>
2024-05-14 15:41:42 +0200

    feat(access): add AccessControl example

---
+++ examples/access-control/src/lib.rs
@@ -0,0 +1,57 @@
+#![cfg_attr(not(test), no_std, no_main)]
+extern crate alloc;
+
+use alloc::vec::Vec;
+
+use alloy_primitives::{Address, U256};
+use contracts::{access::access_control::AccessControl, erc20::ERC20};
+use stylus_sdk::{
+    msg,
+    prelude::{entrypoint, external, sol_storage},
+};
+
+sol_storage! {
+    #[entrypoint]
+    struct Token {
+        #[borrow]
+        ERC20 erc20;
+        #[borrow]
+        AccessControl access;
+    }
+}
+
+#[external]
+#[inherit(ERC20, AccessControl)]
+impl Token {
+    // `keccak256("TRANSFER_ROLE")`
+    const TRANSFER_ROLE: [u8; 32] = [
+        133, 2, 35, 48, 150, 217, 9, 190, 251, 218, 9, 153, 187, 142, 162, 243,
+        166, 190, 60, 19, 139, 159, 191, 0, 55, 82, 164, 200, 188, 232, 111,
+        108,
+    ];
+
+    pub fn constructor(&mut self) -> Result<(), Vec<u8>> {
+        // Make caller the default admin.
+        self.access.grant_role(
+            AccessControl::DEFAULT_ADMIN_ROLE.into(),
+            msg::sender(),
+        )?;
+        Ok(())
+    }
+
+    pub fn make_admin(&mut self, account: Address) -> Result<(), Vec<u8>> {
+        self.access.only_role(AccessControl::DEFAULT_ADMIN_ROLE.into())?;
+        self.access.grant_role(Token::TRANSFER_ROLE.into(), account)?;
+        Ok(())
+    }
+
+    pub fn transfer(
+        &mut self,
+        to: Address,
+        value: U256,
+    ) -> Result<(), Vec<u8>> {
+        self.access.only_role(Token::TRANSFER_ROLE.into())?;
+        self.erc20.transfer(to, value)?;
+        Ok(())
+    }
+}
