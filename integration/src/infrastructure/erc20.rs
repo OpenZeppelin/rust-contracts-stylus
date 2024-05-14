7a6dd9049ef5016ab676370e3cebe137c2886442
Alisander Qoshqosh <qalisander@gmail.com>
2024-05-14 18:41:15 +0400

    ++

--- integration/src/infrastructure/erc20.rs
+++ integration/src/infrastructure/erc20.rs
@@ -20,6 +20,12 @@ abigen!(
         
         function mint(address account, uint256 amount) external
         function burn(uint256 amount) external
+        
+        error ERC20InsufficientBalance(address sender, uint256 balance, uint256 needed)
+        error ERC20InvalidSender(address sender)
+        error ERC20InvalidReceiver(address receiver)
+        error ERC20InsufficientAllowance(address spender, uint256 allowance, uint256 needed)
+        error ERC20InvalidSpender(address spender)
     ]"#
 );
 
