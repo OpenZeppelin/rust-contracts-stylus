075b110d28ccd3048d4980412f952a0f72f82a3c
Alisander Qoshqosh <qalisander@gmail.com>
2024-05-19 00:51:50 +0400

    ++

--- integration/src/erc20.rs
+++ integration/src/erc20.rs
@@ -6,7 +6,7 @@ use crate::infrastructure::{erc20::*, *};
 #[tokio::test]
 async fn mint() -> Result<()> {
     let Infrastructure { alice, bob } = Infrastructure::<Erc20>::new().await?;
-    // TODO: has a nicer support for custom constructors
+    // TODO: have a nicer support for custom constructors
     let _ = alice
         .constructor("MyErc20".to_string(), "MRC".to_string(), U256::from(10))
         .ctx_send()
