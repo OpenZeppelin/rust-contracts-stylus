a8b84be1a81f7b6393bba18dc0f46155f5b2182c
Daniel Bigos <daniel.bigos96@gmail.com>
2024-05-21 11:23:57 +0200

    feat(erc721): add ERC-721 URI Storage extension (#56)
    Resolves #56 
    
    #### PR Checklist
    - [x] Tests
    - [x] Documentation

--- examples/erc721/src/lib.rs
+++ examples/erc721/src/lib.rs
@@ -1,9 +1,13 @@
 #![cfg_attr(not(test), no_main, no_std)]
 extern crate alloc;
 
-use alloc::string::String;
+use alloc::string::{String, ToString};
 
-use contracts::erc721::{extensions::ERC721Metadata, ERC721};
+use alloy_primitives::U256;
+use contracts::erc721::{
+    extensions::{ERC721Metadata, ERC721UriStorage},
+    ERC721,
+};
 use stylus_sdk::prelude::{entrypoint, external, sol_storage};
 
 sol_storage! {
@@ -13,14 +17,16 @@ sol_storage! {
         ERC721 erc721;
         #[borrow]
         ERC721Metadata metadata;
+        #[borrow]
+        ERC721UriStorage uri_storage;
     }
 }
 
 #[external]
-#[inherit(ERC721, ERC721Metadata)]
+#[inherit(ERC721, ERC721Metadata, ERC721UriStorage)]
 impl Token {
-    // We need to properly initialize all Token's attributes.
-    // For that we need to call each attributes' constructor if exists.
+    // We need to properly initialize all of Token's attributes.
+    // For that, we need to call each attribute's constructor if it exists.
     //
     // NOTE: This is a temporary solution for state initialization.
     pub fn constructor(
@@ -30,5 +36,28 @@ impl Token {
         base_uri: String,
     ) {
         self.metadata.constructor(name, symbol, base_uri);
+    }
+
+    // Overrides [`ERC721UriStorage::token_uri`].
+    // Returns the Uniform Resource Identifier (URI) for tokenId token.
+    pub fn token_uri(&self, token_id: U256) -> String {
+        let base = self.metadata.base_uri();
+        let token_uri = self.uri_storage.token_uri(token_id);
+
+        // If there is no base URI, return the token URI.
+        if base.is_empty() {
+            return token_uri;
+        }
+
+        let mut uri = base;
+        // If both are set, concatenate the baseURI and tokenURI (via
+        // string.concat).
+        if !token_uri.is_empty() {
+            uri.push_str(&token_uri);
+        } else {
+            uri.push_str(&token_id.to_string());
+        }
+
+        uri
     }
 }
