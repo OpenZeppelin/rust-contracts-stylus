9bf995daff2229ee9b22d3a20ae07cdf90cbf6f0
Alisander Qoshqosh <qalisander@gmail.com>
2024-05-23 17:37:13 +0400

    ++

--- e2e-tests/test.sh
+++ e2e-tests/test.sh
@@ -57,8 +57,9 @@ get_example_crate_names () {
 export ALICE_PRIV_KEY=${ALICE_PRIV_KEY:-0x5744b91fe94e38f7cde31b0cc83e7fa1f45e31c053d015b9fb8c9ab3298f8a2d}
 export BOB_PRIV_KEY=${BOB_PRIV_KEY:-0xa038232e463efa8ad57de6f88cd3c68ed64d1981daff2dcc015bce7eaf53db9d}
 export RPC_URL=${RPC_URL:-http://localhost:8547}
+NIGHTLY_TOOLCHAIN=${NIGHTLY_TOOLCHAIN:-nightly}
 
-cargo +nightly build --release --target wasm32-unknown-unknown -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort
+cargo +"$NIGHTLY_TOOLCHAIN" build --release --target wasm32-unknown-unknown -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort
 
 # TODO: deploy contracts asynchronously
 for CRATE_NAME in $(get_example_crate_names)
