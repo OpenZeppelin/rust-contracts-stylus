bb3656597ac78d4e6851fc4a112fbf61e536b1a2
Daniel Bigos <daniel.bigos@icloud.com>
2024-05-27 18:35:08 +0200

    docs(erc721): improve docs for ERC-721 Enumerable (#67)

--- contracts/src/erc721/extensions/enumerable.rs
+++ contracts/src/erc721/extensions/enumerable.rs
@@ -8,39 +8,50 @@
 //! custom [`ERC721::balance_of`] logic, such as [`ERC721Consecutive`],
 //! interfere with enumerability and should not be used together
 //! with [`ERC721Enumerable`].
-
 use alloy_primitives::{Address, U256};
 use alloy_sol_types::sol;
 use stylus_proc::{external, sol_storage, SolidityError};
 
 sol! {
-    // TODO
+    /// Indicates an error when an `owner`'s token query
+    /// was out of bounds for `index`.
+    ///
+    /// NOTE: The owner being `Address::ZERO`
+    /// indicates a global out of bounds index.
     #[derive(Debug)]
     #[allow(missing_docs)]
     error ERC721OutOfBoundsIndex(address owner, uint256 index);
 
-    // TODO
+    /// Indicates an error related to batch minting not allowed.
     #[derive(Debug)]
     #[allow(missing_docs)]
     error ERC721EnumerableForbiddenBatchMint();
 }
 
-// TODO
+/// An [`ERC721Enumerable`] extension error.
 #[derive(SolidityError, Debug)]
 pub enum Error {
+    /// Indicates an error when an `owner`'s token query
+    /// was out of bounds for `index`.
+    ///
+    /// NOTE: The owner being `Address::ZERO`
+    /// indicates a global out of bounds index.
     OutOfBoundsIndex(ERC721OutOfBoundsIndex),
+
+    /// Indicates an error related to batch minting not allowed.
     EnumerableForbiddenBatchMint(ERC721EnumerableForbiddenBatchMint),
 }
 
 sol_storage! {
+    /// State of an Enumerable extension.
     pub struct ERC721Enumerable {
-        // TODO
+        /// TODO
         mapping(address => mapping(uint256 => uint256)) _owned_tokens;
-        // TODO
+        /// TODO
         mapping(uint256 => uint256) _owned_tokens_index;
-        // TODO
+        /// TODO
         uint256[] _all_tokens;
-        // TODO
+        /// TODO
         mapping(uint256 => uint256) _all_tokens_index;
     }
 
@@ -48,6 +59,7 @@ sol_storage! {
 
 /// This is an interface of the optional `Enumerable` extension
 /// of the [`ERC721`] standard.
+#[allow(clippy::module_name_repetitions)]
 pub trait IERC721Enumerable {
     // TODO: fn supports_interface (#33)
 
@@ -60,6 +72,11 @@ pub trait IERC721Enumerable {
     /// # Arguments
     ///
     /// * `&self` - Read access to the contract's state.
+    ///
+    /// # Errors
+    ///
+    /// * If an `owner`'s token query is out of bounds for `index`,
+    /// then the error [`Error::OutOfBoundsIndex`] is returned.
     fn token_of_owner_by_index(
         &self,
         owner: Address,
@@ -76,65 +93,72 @@ pub trait IERC721Enumerable {
     /// Returns a token ID at a given `index` of all the tokens
     /// stored by the contract.
     ///
-    /// Use along with totalSupply to enumerate all tokens.
+    /// Use along with [`ERC721::total_supply`] to enumerate all tokens.
     ///
     /// # Arguments
     ///
     /// * `&self` - Read access to the contract's state.
+    ///
+    /// # Errors
+    ///
+    /// * If an `owner`'s token query is out of bounds for `index`,
+    /// then the error [`Error::OutOfBoundsIndex`] is returned.
+
     fn token_by_index(&self, index: U256) -> Result<U256, Error>;
 }
 
 #[external]
 impl IERC721Enumerable for ERC721Enumerable {
-    // TODO
     fn token_of_owner_by_index(
         &self,
-        owner: Address,
-        index: U256,
+        _owner: Address,
+        _index: U256,
     ) -> Result<U256, Error> {
         // TODO
         unimplemented!()
     }
 
-    // TODO
     fn total_supply(&self) -> U256 {
         // TODO
         unimplemented!()
     }
 
-    // TODO
-    fn token_by_index(&self, index: U256) -> Result<U256, Error> {
+    fn token_by_index(&self, _index: U256) -> Result<U256, Error> {
         // TODO
         unimplemented!()
     }
 }
 
 impl ERC721Enumerable {
-    fn _add_token_to_owner_enumeration(&mut self, to: Address, token_id: U256) {
+    fn _add_token_to_owner_enumeration(
+        &mut self,
+        _to: Address,
+        _token_id: U256,
+    ) {
         // TODO
         unimplemented!()
     }
 
-    fn _add_token_to_all_tokens_enumeration(&mut self, token_id: U256) {
+    fn _add_token_to_all_tokens_enumeration(&mut self, _token_id: U256) {
         // TODO
         unimplemented!()
     }
 
     fn _remove_token_from_owner_enumeration(
         &mut self,
-        from: Address,
-        token_id: U256,
+        _from: Address,
+        _token_id: U256,
     ) {
         // TODO
         unimplemented!()
     }
 
-    fn _remove_token_from_all_tokens_enumeration(&mut self, token_id: U256) {
+    fn _remove_token_from_all_tokens_enumeration(&mut self, _token_id: U256) {
         // TODO
         unimplemented!()
     }
 
-    fn _increase_balance(&mut self, account: Address, amount: u128) {
+    fn _increase_balance(&mut self, _account: Address, _amount: u128) {
         // TODO
         unimplemented!()
     }
