9b7fe6674e4e7828b7710d389f5747f9e7f304e7
Alisander Qoshqosh <qalisander@gmail.com>
2024-05-27 11:01:08 +0400

    ++

--- lib/e2e-grip-proc/src/test.rs
+++ lib/e2e-grip-proc/src/test.rs
@@ -1,6 +1,6 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{FnArg, parse_macro_input};
+use syn::{parse_macro_input, FnArg};
 
 /// Shorthand to print nice errors.
 macro_rules! error {
