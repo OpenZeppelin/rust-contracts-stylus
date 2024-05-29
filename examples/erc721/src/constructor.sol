cb93510a2fa3d8fbdc88bbd86c095651ab9e6d90
Alexander Gonzalez <alexfertel97@gmail.com>
2024-05-29 13:06:41 +0200

    ref: trim down erc721 example so that it's deployable on stable

--- examples/erc721/src/constructor.sol
+++ examples/erc721/src/constructor.sol
@@ -13,11 +13,8 @@ contract Erc721Example {
 
     mapping(uint256 => string) _tokenUris;
 
-    bool _paused;
-
     constructor(string memory name_, string memory symbol_) {
         _name = name_;
         _symbol = symbol_;
-        _paused = false;
     }
 }
