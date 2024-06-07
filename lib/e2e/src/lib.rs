af494148867417bbdcdd37c0060b33a1d4493238
Daniel Bigos <daniel.bigos@icloud.com>
2024-06-07 13:23:07 +0200

    ref: rename module with assertions (#88)

--- lib/e2e/src/lib.rs
+++ lib/e2e/src/lib.rs
@@ -1,11 +1,11 @@
-mod assert;
+mod assertions;
 mod deploy;
 mod environment;
 mod project;
 mod system;
 mod user;
 
-pub use assert::{Assert, Emits};
+pub use assertions::{Assert, Emits};
 pub use deploy::deploy;
 pub use e2e_proc::test;
 pub use system::{provider, Provider, Signer};
