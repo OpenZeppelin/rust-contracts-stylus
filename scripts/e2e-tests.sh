7221d4671c2769a8597644c25b5afcd167bcfe70
Alexander Gonzalez <alexfertel97@gmail.com>
2024-06-18 14:48:31 +0200

    fix: revert to using nightly in ci

--- scripts/e2e-tests.sh
+++ scripts/e2e-tests.sh
@@ -5,9 +5,10 @@ MYDIR=$(realpath "$(dirname "$0")")
 cd "$MYDIR"
 cd ..
 
-cargo build --release --target wasm32-unknown-unknown
+NIGHTLY_TOOLCHAIN=${NIGHTLY_TOOLCHAIN:-nightly}
+cargo +"$NIGHTLY_TOOLCHAIN" build --release --target wasm32-unknown-unknown -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort
 
 export RPC_URL=http://localhost:8547
 # We should use stable here once nitro-testnode is updated and the contracts fit
 # the size limit. Work tracked [here](https://github.com/OpenZeppelin/rust-contracts-stylus/issues/87)
-cargo test --features std,e2e --test "*"
+cargo +"$NIGHTLY_TOOLCHAIN" test --features std,e2e --test "*"
