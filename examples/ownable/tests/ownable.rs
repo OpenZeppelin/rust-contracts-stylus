7a83a37d500e8ed446da9fbe49352b0278bf3ad8
Alexander Gonzalez <alexfertel97@gmail.com>
2024-06-20 17:30:31 +0200

    fix: check previous block for events

--- examples/ownable/tests/ownable.rs
+++ examples/ownable/tests/ownable.rs
@@ -45,11 +45,12 @@ async fn emits_ownership_transfer_during_construction(
 ) -> Result<()> {
     let alice_addr = alice.address();
 
+    deploy(&alice, alice_addr).await?;
+
+    let block = alice.signer.get_block_number().await?;
     let filter = Filter::new()
         .event_signature(OwnershipTransferred::SIGNATURE_HASH)
-        .from_block(BlockNumberOrTag::Latest);
-
-    deploy(&alice, alice_addr).await?;
+        .from_block(BlockNumberOrTag::Number(block - 1));
 
     let logs = alice.signer.get_logs(&filter).await?;
     let emitted = logs[0].log_decode::<OwnershipTransferred>()?.inner.data;
@@ -64,7 +65,7 @@ async fn emits_ownership_transfer_during_construction(
 
 #[e2e::test]
 async fn rejects_zero_address_initial_owner(alice: User) -> Result<()> {
-    let err = deploy(&alice, alice.address())
+    let err = deploy(&alice, Address::ZERO)
         .await
         .expect_err("should not deploy due to `OwnableInvalidOwner`");
 
