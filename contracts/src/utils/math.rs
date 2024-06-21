14369c8cfe7f076314f290e8378c1715acbec62c
Alisander Qoshqosh <qalisander@gmail.com>
2024-06-21 11:43:55 +0400

    ++

--- contracts/src/utils/math.rs
+++ contracts/src/utils/math.rs
@@ -1,18 +1,31 @@
 use alloy_primitives::{uint, U256};
 
-// TODO#q: use more smart way
+// TODO#q: add those helpers as an extension to U256
 
 /// Returns the square root of a number. If the number is not a perfect
 /// square, the value is rounded towards zero.
 /// This method is based on Newton's method for computing square roots; the
 /// algorithm is restricted to only using integer operations.
 pub fn sqrt(a: U256) -> U256 {
-    // TODO#q: refactor this
     let one = uint!(1_U256);
     if a <= one {
         return a;
     }
 
+    // In this function, we use Newton's method to get a root of `f(x) := x² -
+    // a`. It involves building a sequence x_n that converges toward
+    // sqrt(a). For each iteration x_n, we also define the error between the
+    // current value as `ε_n = | x_n - sqrt(a) |`.
+    //
+    // For our first estimation, we consider `e` the smallest power of 2 which
+    // is bigger than the square root of the target. (i.e. `2**(e-1) ≤
+    // sqrt(a) < 2**e`). We know that `e ≤ 128` because `(2¹²⁸)² = 2²⁵⁶` is
+    // bigger than any uint256.
+    //
+    // By noticing that
+    // `2**(e-1) ≤ sqrt(a) < 2**e → (2**(e-1))² ≤ a < (2**e)² → 2**(2*e-2) ≤ a <
+    // 2**(2*e)` we can deduce that `e - 1` is `log2(a) / 2`. We can thus
+    // compute `x_n = 2**(e-1)` using a method similar to the msb function.
     let mut aa = a;
     let mut xn = one;
 
@@ -44,32 +57,88 @@ pub fn sqrt(a: U256) -> U256 {
         xn <<= 1;
     }
 
-    xn = (uint!(3_U256) * xn) >> 1;
+    // We now have x_n such that `x_n = 2**(e-1) ≤ sqrt(a) < 2**e = 2 * x_n`.
+    // This implies ε_n ≤ 2**(e-1).
+    //
+    // We can refine our estimation by noticing that the middle of that interval
+    // minimizes the error. If we move x_n to equal 2**(e-1) + 2**(e-2),
+    // then we reduce the error to ε_n ≤ 2**(e-2). This is going to be our
+    // x_0 (and ε_0)
+    xn = (uint!(3_U256) * xn) >> 1; // ε_0 := | x_0 - sqrt(a) | ≤ 2**(e-2)
 
-    xn = (xn + a / xn) >> 1;
-    xn = (xn + a / xn) >> 1;
-    xn = (xn + a / xn) >> 1;
-    xn = (xn + a / xn) >> 1;
-    xn = (xn + a / xn) >> 1;
-    xn = (xn + a / xn) >> 1;
+    // From here, Newton's method give us:
+    // x_{n+1} = (x_n + a / x_n) / 2
+    //
+    // One should note that:
+    // x_{n+1}² - a = ((x_n + a / x_n) / 2)² - a
+    //              = ((x_n² + a) / (2 * x_n))² - a
+    //              = (x_n⁴ + 2 * a * x_n² + a²) / (4 * x_n²) - a
+    //              = (x_n⁴ + 2 * a * x_n² + a² - 4 * a * x_n²) / (4 * x_n²)
+    //              = (x_n⁴ - 2 * a * x_n² + a²) / (4 * x_n²)
+    //              = (x_n² - a)² / (2 * x_n)²
+    //              = ((x_n² - a) / (2 * x_n))²
+    //              ≥ 0
+    // Which proves that for all n ≥ 1, sqrt(a) ≤ x_n
+    //
+    // This gives us the proof of quadratic convergence of the sequence:
+    // ε_{n+1} = | x_{n+1} - sqrt(a) |
+    //         = | (x_n + a / x_n) / 2 - sqrt(a) |
+    //         = | (x_n² + a - 2*x_n*sqrt(a)) / (2 * x_n) |
+    //         = | (x_n - sqrt(a))² / (2 * x_n) |
+    //         = | ε_n² / (2 * x_n) |
+    //         = ε_n² / | (2 * x_n) |
+    //
+    // For the first iteration, we have a special case where x_0 is known:
+    // ε_1 = ε_0² / | (2 * x_0) |
+    //     ≤ (2**(e-2))² / (2 * (2**(e-1) + 2**(e-2)))
+    //     ≤ 2**(2*e-4) / (3 * 2**(e-1))
+    //     ≤ 2**(e-3) / 3
+    //     ≤ 2**(e-3-log2(3))
+    //     ≤ 2**(e-4.5)
+    //
+    // For the following iterations, we use the fact that, 2**(e-1) ≤ sqrt(a) ≤
+    // x_n: ε_{n+1} = ε_n² / | (2 * x_n) |
+    //         ≤ (2**(e-k))² / (2 * 2**(e-1))
+    //         ≤ 2**(2*e-2*k) / 2**e
+    //         ≤ 2**(e-2*k)
+    xn = (xn + a / xn) >> 1; // ε_1 := | x_1 - sqrt(a) | ≤ 2**(e-4.5)  -- special case, see above
+    xn = (xn + a / xn) >> 1; // ε_2 := | x_2 - sqrt(a) | ≤ 2**(e-9)    -- general case with k = 4.5
+    xn = (xn + a / xn) >> 1; // ε_3 := | x_3 - sqrt(a) | ≤ 2**(e-18)   -- general case with k = 9
+    xn = (xn + a / xn) >> 1; // ε_4 := | x_4 - sqrt(a) | ≤ 2**(e-36)   -- general case with k = 18
+    xn = (xn + a / xn) >> 1; // ε_5 := | x_5 - sqrt(a) | ≤ 2**(e-72)   -- general case with k = 36
+    xn = (xn + a / xn) >> 1; // ε_6 := | x_6 - sqrt(a) | ≤ 2**(e-144)  -- general case with k = 72
 
+    // Because e ≤ 128 (as discussed during the first estimation phase), we know
+    // have reached a precision ε_6 ≤ 2**(e-144) < 1. Given we're operating
+    // on integers, then we can ensure that xn is now either sqrt(a) or
+    // sqrt(a) + 1.
     xn - U256::from(xn > a / xn)
 }
 
 /// Returns the average of two numbers. The result is rounded towards zero.
 pub fn average(a: U256, b: U256) -> U256 {
+    // (a + b) / 2 can overflow.
     (a & b) + (a ^ b) / uint!(2_U256)
 }
 
 #[cfg(all(test, feature = "std"))]
 mod tests {
-    use alloy_primitives::uint;
+    use alloy_primitives::{private::proptest::proptest, uint, U256, U512};
 
-    use crate::utils::math::sqrt;
+    use crate::utils::math::{average, sqrt};
 
     #[test]
     fn check_sqrt() {
-        // TODO#q: use proptest
-        assert_eq!(sqrt(uint!(27_U256)), uint!(5_U256));
+        proptest!(|(value: U256)| {
+            assert_eq!(sqrt(value), value.root(2));
+        });
+    }
+
+    #[test]
+    fn check_average() {
+        proptest!(|(a: U256, b: U256)| {
+            let expected = (U512::from(a) + U512::from(b)) / uint!(2_U512);
+            assert_eq!(average(a, b), U256::from(expected));
+        });
     }
 }
