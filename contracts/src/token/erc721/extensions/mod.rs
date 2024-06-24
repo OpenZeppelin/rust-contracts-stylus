2c9a3bd4dc3c76a85e8cdca2b591128be78b6e01
Alexander González <alexfertel97@gmail.com>
2024-06-24 11:18:19 +0200

    fix: remove most feature flags (#151)
    It turns out that we don't need to defensibly hide code behind feature
    flags, the rust compiler does a good job, so we remove most feature
    flags since they don't impact binary size.

--- contracts/src/token/erc721/extensions/mod.rs
+++ contracts/src/token/erc721/extensions/mod.rs
@@ -1,29 +1,10 @@
 //! Common extensions to the ERC-721 standard.
+pub mod burnable;
+pub mod enumerable;
+pub mod metadata;
+pub mod uri_storage;
 
-cfg_if::cfg_if! {
-    if #[cfg(any(test, feature = "erc721_burnable"))] {
-        pub mod burnable;
-        pub use burnable::IErc721Burnable;
-    }
-}
-
-cfg_if::cfg_if! {
-    if #[cfg(any(test, feature = "erc721_enumerable"))] {
-        pub mod enumerable;
-        pub use enumerable::{IErc721Enumerable, Erc721Enumerable};
-    }
-}
-
-cfg_if::cfg_if! {
-    if #[cfg(any(test, feature = "erc721_metadata"))] {
-        pub mod metadata;
-        pub use metadata::{IErc721Metadata, Erc721Metadata};
-    }
-}
-
-cfg_if::cfg_if! {
-    if #[cfg(any(test, feature = "erc721_uri_storage"))] {
-        pub mod uri_storage;
-        pub use uri_storage::Erc721UriStorage;
-    }
-}
+pub use burnable::IErc721Burnable;
+pub use enumerable::{Erc721Enumerable, IErc721Enumerable};
+pub use metadata::{Erc721Metadata, IErc721Metadata};
+pub use uri_storage::Erc721UriStorage;
