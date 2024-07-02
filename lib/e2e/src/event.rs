343fe53ae3d67ca23f8b93cf7d073d223e652da9
alexfertel <alexfertel97@gmail.com>
2024-07-02 14:27:47 +0200

    feat(e2e): enable checking whether an event was not emitted (#177)
    Small refactor to enable checking an event does *not* get emitted.
    
    I'm currently working on the e2e tests for `AccessControl` and had the
    need to check that an event was not emitted. However, with the current
    API of `receipt.emits`, the assertion occurs inside the `emits`
    function, which does not let me do something like
    `assert!(!receipt.emits(...));`.
    
    This PR enables this by delegating the assertion to the caller.
    
    This also makes the API more consistent with the Error e2e API.

--- lib/e2e/src/event.rs
+++ lib/e2e/src/event.rs
@@ -3,25 +3,21 @@ use alloy::{rpc::types::eth::TransactionReceipt, sol_types::SolEvent};
 /// Extension trait for asserting an event gets emitted.
 pub trait EventExt<E> {
     /// Asserts the contract emitted the `expected` event.
-    fn emits(&self, expected: E);
+    fn emits(&self, expected: E) -> bool;
 }
 
 impl<E> EventExt<E> for TransactionReceipt
 where
     E: SolEvent,
     E: PartialEq,
-    E: std::fmt::Debug,
 {
-    fn emits(&self, expected: E) {
+    fn emits(&self, expected: E) -> bool {
         // Extract all events that are the expected type.
-        let emitted = self
-            .inner
+        self.inner
             .logs()
             .iter()
             .filter_map(|log| log.log_decode().ok())
             .map(|log| log.inner.data)
-            .any(|event| expected == event);
-
-        assert!(emitted, "Event {expected:?} not emitted");
+            .any(|event| expected == event)
     }
 }
