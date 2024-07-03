b5990447b60abffd5bc553376ac3c7c28ff227a1
Alisander Qoshqosh <qalisander@gmail.com>
2024-07-03 20:11:52 +0400

    add test scratch for consecutive

---
+++ examples/erc721-consecutive/tests/abi.rs
@@ -0,0 +1,38 @@
+#![allow(dead_code)]
+use alloy::sol;
+
+sol!(
+    #[sol(rpc)]
+   contract Erc721 {
+        function balanceOf(address owner) external view returns (uint256 balance);
+        function ownerOf(uint256 tokenId) external view returns (address ownerOf);
+        function safeTransferFrom(address from, address to, uint256 tokenId) external;
+        function safeTransferFrom(address from, address to, uint256 tokenId, bytes calldata data) external;
+        function transferFrom(address from, address to, uint256 tokenId) external;
+        function approve(address to, uint256 tokenId) external;
+        function setApprovalForAll(address operator, bool approved) external;
+        function getApproved(uint256 tokenId) external view returns (address);
+        function isApprovedForAll(address owner, address operator) external view returns (bool);
+
+        function burn(uint256 tokenId) external;
+        function mintConsecutive(address to, uint128 batchSize) external;
+
+        error ERC721InvalidOwner(address owner);
+        error ERC721NonexistentToken(uint256 tokenId);
+        error ERC721IncorrectOwner(address sender, uint256 tokenId, address owner);
+        error ERC721InvalidSender(address sender);
+        error ERC721InvalidReceiver(address receiver);
+        error ERC721InsufficientApproval(address operator, uint256 tokenId);
+        error ERC721InvalidApprover(address approver);
+        error ERC721InvalidOperator(address operator);
+
+        #[derive(Debug, PartialEq)]
+        event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+
+        #[derive(Debug, PartialEq)]
+        event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
+
+        #[derive(Debug, PartialEq)]
+        event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
+    }
+);
---
+++ examples/erc721-consecutive/tests/erc721.rs
@@ -0,0 +1,29 @@
+#![cfg(feature = "e2e")]
+
+use alloy::primitives::{Address, U256};
+use e2e::{Account, EventExt, Revert};
+
+use crate::abi::Erc721;
+
+mod abi;
+
+fn random_token_id() -> U256 {
+    let num: u32 = rand::random();
+    U256::from(num)
+}
+
+async fn deploy(rpc_url: &str, private_key: &str) -> eyre::Result<Address> {
+    e2e::deploy(rpc_url, private_key, None).await
+}
+
+#[e2e::test]
+async fn constructs(alice: Account) -> eyre::Result<()> {
+    let contract_addr = deploy(alice.url(), &alice.pk()).await?;
+    let contract = Erc721::new(contract_addr, &alice.wallet);
+
+    let alice_addr = alice.address();
+    let res = contract.mintConsecutive(alice_addr, 10_u128).call().await?;
+
+    todo!();
+    Ok(())
+}
