91a0fa3bf6d837edaabaf452fe06d2365e5c0f36
Nwele Uchenna (developeruche) <nweleuchenna2004@gmail.com>
2024-07-06 12:40:25 +0100

    resolves CI

--- contracts/src/utils/mod.rs
+++ contracts/src/utils/mod.rs
@@ -1,9 +1,9 @@
 //! Common Smart Contracts utilities.
 pub mod math;
 pub mod metadata;
+pub mod nonces;
 pub mod pausable;
 pub mod structs;
-pub mod nonces;
 
 pub use metadata::Metadata;
 pub use pausable::Pausable;
--- contracts/src/utils/nonces.rs
+++ contracts/src/utils/nonces.rs
@@ -1,14 +1,13 @@
 //! Nonces Contract
-//! 
-//! Contract module which provides functionalities for tracking nonces for addresses.
-//! 
+//!
+//! Contract module which provides functionalities for tracking nonces for
+//! addresses.
+//!
 //! Note: Nonce will only increment.
 
 use alloy_primitives::{Address, U256};
 use alloy_sol_types::sol;
 use stylus_proc::{external, sol_storage, SolidityError};
-use stylus_sdk::{evm, msg};
-
 
 sol! {
     /// The nonce used for an `account` is not the expected current nonce.
@@ -36,64 +35,70 @@ sol_storage! {
 #[external]
 impl Nonces {
     /// Returns the unuse nonce for the given `account`.
-    /// 
+    ///
     /// # Arguments
-    /// 
+    ///
     /// - `account` - The address for which to return the nonce.
     /// * `&self` - Read access to the contract's state.
     fn nonce(&self, owner: Address) -> U256 {
         self._nonces.get(owner)
     }
-    
+
     /// Consumes a nonce for the given `account`.
-    /// 
+    ///
     /// # Arguments
-    /// 
+    ///
     /// - `account` - The address for which to consume the nonce.
     /// * `&mut self` - Write access to the contract's state.
-    /// 
-    /// * Returns the current nonce for the given `account` and increments the nonce.
+    ///
+    /// * Returns the current nonce for the given `account` and increments the
+    ///   nonce.
     fn use_nonce(&mut self, owner: Address) -> Result<U256, Error> {
         let nonce = self._nonces.get(owner);
         self._nonces.setter(owner).set(nonce + U256::from(1u32));
-        
+
         Ok(nonce)
     }
-    
-    /// Same as `use_nonce` but checking that the `nonce` is the next valid for the owner.
-    /// 
+
+    /// Same as `use_nonce` but checking that the `nonce` is the next valid for
+    /// the owner.
+    ///
     /// # Arguments
-    /// 
+    ///
     /// - `account` - The address for which to consume the nonce.
     /// - `nonce` - The nonce to consume.
     /// * `&mut self` - Write access to the contract's state.
-    fn use_checked_nonce(&mut self, owner: Address, nonce: U256) -> Result<(), Error> {
+    fn use_checked_nonce(
+        &mut self,
+        owner: Address,
+        nonce: U256,
+    ) -> Result<(), Error> {
         let current_nonce = self._nonces.get(owner);
-        
+
         if nonce != current_nonce {
             return Err(Error::InvalidAccountNonce(InvalidAccountNonce {
                 account: owner,
                 currentNonce: current_nonce,
             }));
         }
-        
+
         self._nonces.setter(owner).set(nonce + U256::from(1u32));
-        
+
         Ok(())
     }
 }
 
 #[cfg(all(test, feature = "std"))]
 mod tests {
-    use alloy_primitives::{Address, U256, address};
-    use crate::utils::nonces::Nonces;
+    use alloy_primitives::{address, Address, U256};
     use alloy_sol_types::sol;
-    
-    
+
+    use crate::utils::nonces::Nonces;
+
     #[motsu::test]
     fn test_initiate_nonce(contract: Nonces) {
         let owner = address!("d8da6bf26964af9d7eed9e03e53415d37aa96045");
-        
+
         assert_eq!(contract.nonce(owner), U256::from(0u32));
     }
 }
