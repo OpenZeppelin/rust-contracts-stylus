cbfa6f1e0b195f0cc42e33835652a587d74f1ad1
Alexander Gonzalez <alexfertel97@gmail.com>
2024-07-09 10:09:43 +0200

    fix: run merkle proof benches concurrently

--- benches/src/main.rs
+++ benches/src/main.rs
@@ -1,8 +1,12 @@
-use benches::{access_control, erc20};
+use benches::{access_control, erc20, merkle_proofs};
 
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
-    let _ = tokio::join!(erc20::bench(), access_control::bench());
+    let _ = tokio::join!(
+        erc20::bench(),
+        access_control::bench(),
+        merkle_proofs::bench()
+    );
 
     Ok(())
 }
