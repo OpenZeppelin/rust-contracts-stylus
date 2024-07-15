3eab1b88b6b0509e5d8f9470734cef0601625aec
Alexander Gonzalez <alexfertel97@gmail.com>
2024-07-15 15:47:45 +0200

    wip

--- benches/src/main.rs
+++ benches/src/main.rs
@@ -2,9 +2,11 @@ use benches::{access_control, erc20, merkle_proofs};
 
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
-    erc20::bench().await?;
-    access_control::bench().await?;
-    merkle_proofs::bench().await?;
+    let _ = tokio::join!(
+        erc20::bench(),
+        access_control::bench(),
+        merkle_proofs::bench()
+    );
 
     Ok(())
 }
