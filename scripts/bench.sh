d6864ebd7a5409f97723f586c3a909321f4aa002
Alexander Gonzalez <alexfertel97@gmail.com>
2024-07-18 11:54:59 +0200

    fix: add proper permissions

--- scripts/bench.sh
+++ scripts/bench.sh
@@ -9,4 +9,7 @@ NIGHTLY_TOOLCHAIN=${NIGHTLY_TOOLCHAIN:-nightly}
 cargo +"$NIGHTLY_TOOLCHAIN" build --release --target wasm32-unknown-unknown -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort
 
 export RPC_URL=http://localhost:8547
+
+echo "Running benches..."
 cargo run --release -p benches
+echo "Finished running benches!"
