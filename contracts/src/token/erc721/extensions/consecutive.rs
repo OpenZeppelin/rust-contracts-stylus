901a5d7909a029b5a949545c29f69afa2cb5b078
Daniel Bigos <daniel.bigos@openzeppelin.com>
2024-08-23 14:05:02 +0200

    ref: code cleanup (#257)
    Cleanup of existing warnings.

--- contracts/src/token/erc721/extensions/consecutive.rs
+++ contracts/src/token/erc721/extensions/consecutive.rs
@@ -27,9 +27,7 @@ use alloc::vec;
 use alloy_primitives::{uint, Address, U256};
 use alloy_sol_types::sol;
 use stylus_proc::{external, sol_storage, SolidityError};
-use stylus_sdk::{
-    abi::Bytes, call::MethodError, evm, msg, prelude::TopLevelStorage,
-};
+use stylus_sdk::{abi::Bytes, evm, msg, prelude::TopLevelStorage};
 
 use crate::{
     token::{
@@ -77,6 +75,7 @@ sol! {
     /// * `to_token_id` - Last token being transferred.
     /// * `from_address` - Address from which tokens will be transferred.
     /// * `to_address` - Address where the tokens will be transferred to.
+    #[allow(missing_docs)]
     event ConsecutiveTransfer(
         uint256 indexed from_token_id,
         uint256 to_token_id,
