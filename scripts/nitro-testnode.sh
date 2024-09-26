e84a57f346db3eff51f005f4dc9992e932dd652b
Alisander Qoshqosh <qalisander@gmail.com>
2024-09-26 19:57:29 +0400

    ++

--- scripts/nitro-testnode.sh
+++ scripts/nitro-testnode.sh
@@ -51,7 +51,7 @@ then
   git clone --recurse-submodules https://github.com/OffchainLabs/nitro-testnode.git
   cd ./nitro-testnode || exit
   git pull origin release --recurse-submodules
-  git checkout 43861b001713db3526b74e905e383d41e9272760 || exit
+  git checkout d4244cd5c2cb56ca3d11c23478ef9642f8ebf472 || exit
 
   ./test-node.bash --no-run --init || exit
 fi
