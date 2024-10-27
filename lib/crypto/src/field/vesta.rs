617f1416e3799dc38b3f0fd93b6e5aaf6543f3f5
Alisander Qoshqosh <qalisander@gmail.com>
2024-10-28 01:46:27 +0400

    ++

--- lib/crypto/src/field/vesta.rs
+++ lib/crypto/src/field/vesta.rs
@@ -1,3 +1,5 @@
+use crypto_bigint::Uint;
+
 use crate::field::fp::{Fp256, FpConfig};
 
 /*
@@ -10,26 +12,44 @@ pub struct FqConfig;
 pub struct FieldConfig;
 pub type FpVesta = Fp256<FieldConfig>;
 
-use crate::{field::fp::Fp, BigInt};
+use crate::field::fp::Fp;
+
 #[automatically_derived]
 impl FpConfig<4usize> for FieldConfig {
-    // TODO#q: convert from number
     const GENERATOR: Fp<FieldConfig, 4> = {
-        let (is_positive, limbs) = (
-            true,
-            [
-                12037607305579515999u64,
-                11221139188353527881u64,
-                11411081306099606126u64,
-                3307517586042601304u64,
-            ],
-        );
-        Fp::from_sign_and_limbs(is_positive, &limbs)
+        Fp::new_unchecked(Uint::<4>::from_words([
+            12037607305579515999u64,
+            11221139188353527881u64,
+            11411081306099606126u64,
+            3307517586042601304u64,
+        ]))
     };
-    const MODULUS: BigInt<4> = BigInt([
+    const MODULUS: Uint<4> = Uint::<4>::from_words([
         10108024940646105089u64,
         2469829653919213789u64,
         0u64,
         4611686018427387904u64,
     ]);
 }
+
+// #[automatically_derived]
+// impl FpConfig<4usize> for FieldConfig {
+//     const GENERATOR: Fp<FieldConfig, 4> = {
+//         let (is_positive, limbs) = (
+//             true,
+//             [
+//                 12037607305579515999u64,
+//                 11221139188353527881u64,
+//                 11411081306099606126u64,
+//                 3307517586042601304u64,
+//             ],
+//         );
+//         Fp::from_sign_and_limbs(is_positive, &limbs)
+//     };
+//     const MODULUS: BigInt<4> = BigInt([
+//         10108024940646105089u64,
+//         2469829653919213789u64,
+//         0u64,
+//         4611686018427387904u64,
+//     ]);
+// }
