2f531143d912054da0a992c5a458a5f66cb7b7bb
Nenad <nenad.misic@openzeppelin.com>
2024-11-07 12:56:19 +0100

    remove empty ctor from Erc1155Example

--- examples/erc1155/src/constructor.sol
+++ examples/erc1155/src/constructor.sol
@@ -4,6 +4,4 @@ pragma solidity ^0.8.24;
 contract Erc1155Example {
     mapping(address => mapping(uint256 => uint256)) private _balanceOf;
     mapping(address => mapping(address => bool)) private _isApprovedForAll;
-
-    constructor() {}
 }
