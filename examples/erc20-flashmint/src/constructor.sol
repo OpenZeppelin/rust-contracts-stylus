f8cdcf2ad8b9dadae1257daf1c9ba7b00a38b9a1
Angry Coding <mailidanielifechukwu@gmail.com>
2024-11-16 18:36:23 +0000

    chore: abi and constructor

---
+++ examples/erc20-flashmint/src/constructor.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
+
+contract Erc20FlashmintExample {
+     mapping(address account => uint256) private _balances;
+    mapping(address account => mapping(address spender => uint256))
+        private _allowances;
+    uint256 private _totalSupply;
+    uint256 private _flash_fee_amount;
+    address private _flash_fee_receiver_address;
+    constructor(address flash_fee_receiver_address_, uint256 _flash_fee_amount_) {
+        _flash_fee_receiver_address = flash_fee_receiver_address_;
+        _flash_fee_amount = _flash_fee_amount_;
+    }
+}
