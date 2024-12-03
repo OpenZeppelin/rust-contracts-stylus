91387c3b24df51983fed4d80b2e80970452b07c4
0xNeshi <nenad.misic@openzeppelin.com>
2024-12-03 12:49:41 +0100

    ref: simplify erc1155-metadata-uri example

--- examples/erc1155-metadata-uri/src/lib.rs
+++ examples/erc1155-metadata-uri/src/lib.rs
@@ -1,20 +1,15 @@
 #![cfg_attr(not(test), no_main)]
 extern crate alloc;
 
-use alloc::vec::Vec;
-
-use alloy_primitives::{Address, FixedBytes, U256};
+use alloy_primitives::{FixedBytes, U256};
 use openzeppelin_stylus::{
     token::erc1155::{
-        extensions::{Erc1155MetadataUri, Erc1155UriStorage, IErc1155Burnable},
+        extensions::{Erc1155MetadataUri, Erc1155UriStorage},
         Erc1155,
     },
     utils::introspection::erc165::IErc165,
 };
-use stylus_sdk::{
-    abi::Bytes,
-    prelude::{entrypoint, public, sol_storage},
-};
+use stylus_sdk::prelude::{entrypoint, public, sol_storage};
 
 sol_storage! {
     #[entrypoint]
@@ -30,27 +25,6 @@ sol_storage! {
 #[public]
 #[inherit(Erc1155, Erc1155MetadataUri)]
 impl Erc1155MetadataUriExample {
-    pub fn mint(
-        &mut self,
-        to: Address,
-        token_id: U256,
-        amount: U256,
-        data: Bytes,
-    ) -> Result<(), Vec<u8>> {
-        self.erc1155._mint(to, token_id, amount, &data)?;
-        Ok(())
-    }
-
-    fn burn(
-        &mut self,
-        account: Address,
-        token_id: U256,
-        value: U256,
-    ) -> Result<(), Vec<u8>> {
-        self.erc1155.burn(account, token_id, value)?;
-        Ok(())
-    }
-
     pub fn supports_interface(interface_id: FixedBytes<4>) -> bool {
         Erc1155::supports_interface(interface_id)
             || Erc1155MetadataUri::supports_interface(interface_id)
