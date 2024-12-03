0f694feacae2df64dbaf258895b0d1dd09a5a48c
0xNeshi <nenad.misic@openzeppelin.com>
2024-12-03 08:32:07 +0100

    ref: remove erc1155-specific funcs from benches for supply ext.

--- benches/src/erc1155_supply.rs
+++ benches/src/erc1155_supply.rs
@@ -16,12 +16,6 @@ use crate::{
 sol!(
     #[sol(rpc)]
     contract Erc1155Supply {
-        function safeTransferFrom(address from, address to, uint256 id, uint256 value, bytes memory data) external;
-        function safeBatchTransferFrom(address from, address to, uint256[] memory ids, uint256[] memory values, bytes memory data) external;
-        function mint(address to, uint256 id, uint256 amount, bytes memory data) external;
-        function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data) external;
-        function burn(address account, uint256 id, uint256 value) external;
-        function burnBatch(address account, uint256[] memory ids, uint256[] memory values) external;
         function totalSupply(uint256 id) external view returns (uint256);
         function totalSupply() external view returns (uint256);
         function exists(uint256 id) external view returns (bool);
@@ -46,48 +40,25 @@ pub async fn run_with(
     cache_opt: CacheOpt,
 ) -> eyre::Result<Vec<FunctionReport>> {
     let alice = Account::new().await?;
-    let alice_addr = alice.address();
     let alice_wallet = ProviderBuilder::new()
         .network::<AnyNetwork>()
         .with_recommended_fillers()
         .wallet(EthereumWallet::from(alice.signer.clone()))
         .on_http(alice.url().parse()?);
 
-    let bob = Account::new().await?;
-    let bob_addr = bob.address();
-
     let contract_addr = deploy(&alice, cache_opt).await?;
 
     let contract = Erc1155Supply::new(contract_addr, &alice_wallet);
 
-    let token_1 = uint!(1_U256);
-    let token_2 = uint!(2_U256);
-    let token_3 = uint!(3_U256);
-    let token_4 = uint!(4_U256);
-
-    let value_1 = uint!(100_U256);
-    let value_2 = uint!(200_U256);
-    let value_3 = uint!(300_U256);
-    let value_4 = uint!(400_U256);
-
-    let ids = vec![token_1, token_2, token_3, token_4];
-    let values = vec![value_1, value_2, value_3, value_4];
-
-    let data: alloy_primitives::Bytes = vec![].into();
+    let token = uint!(1_U256);
 
     // IMPORTANT: Order matters!
     use Erc1155Supply::*;
     #[rustfmt::skip]
     let receipts = vec![
-        (mintCall::SIGNATURE, receipt!(contract.mint(alice_addr, token_1, value_1, data.clone()))?),
-        (mintBatchCall::SIGNATURE, receipt!(contract.mintBatch(alice_addr, ids.clone(), values.clone(), data.clone()))?),
-        (existsCall::SIGNATURE, receipt!(contract.exists(token_1))?),
-        (totalSupply_0Call::SIGNATURE, receipt!(contract.totalSupply_0(token_1))?),
+        (existsCall::SIGNATURE, receipt!(contract.exists(token))?),
+        (totalSupply_0Call::SIGNATURE, receipt!(contract.totalSupply_0(token))?),
         (totalSupply_1Call::SIGNATURE, receipt!(contract.totalSupply_1())?),
-        (safeTransferFromCall::SIGNATURE, receipt!(contract.safeTransferFrom(alice_addr, bob_addr, token_1, value_1, data.clone()))?),
-        (safeBatchTransferFromCall::SIGNATURE, receipt!(contract.safeBatchTransferFrom(alice_addr, bob_addr, ids.clone(), values.clone(), data.clone()))?),
-        (burnCall::SIGNATURE, receipt!(contract.burn(bob_addr, token_1, value_1))?),
-        (burnBatchCall::SIGNATURE, receipt!(contract.burnBatch(bob_addr, ids, values))?),
     ];
 
     receipts
