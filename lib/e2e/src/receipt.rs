715090c0b36d6fe5f8c5ec3448f13623a8316049
0xNeshi <nenad.misic@openzeppelin.com>
2024-12-05 12:30:21 +0100

    docs: add missing Errors sections in lib

--- lib/e2e/src/receipt.rs
+++ lib/e2e/src/receipt.rs
@@ -7,6 +7,10 @@ use eyre::ContextCompat;
 /// Extension trait to recover address of the contract that was deployed.
 pub trait ReceiptExt {
     /// Returns the address of the contract from the [`TransactionReceipt`].
+    ///
+    /// # Errors
+    ///
+    /// May fail if there's no contract address.
     fn address(&self) -> eyre::Result<Address>;
 }
 
