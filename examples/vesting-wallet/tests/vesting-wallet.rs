3a6e6bd3ffec2f428c7c4fc7b8cc4f37440ff076
Alisander Qoshqosh <qalisander@gmail.com>
2024-12-07 19:08:45 +0400

    retrieve tx hashes for block timestamp

--- examples/vesting-wallet/tests/vesting-wallet.rs
+++ examples/vesting-wallet/tests/vesting-wallet.rs
@@ -39,7 +39,7 @@ fn ctr(
 async fn block_timestamp(account: &Account) -> eyre::Result<u64> {
     let timestamp = account
         .wallet
-        .get_block(BlockId::latest(), BlockTransactionsKind::Full)
+        .get_block(BlockId::latest(), BlockTransactionsKind::Hashes)
         .await?
         .expect("latest block should exist")
         .header
