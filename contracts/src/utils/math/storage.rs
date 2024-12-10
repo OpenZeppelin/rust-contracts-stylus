bbe422822354b4b06e592571cc19b4c70029ed81
Alisander Qoshqosh <qalisander@gmail.com>
2024-12-10 16:21:38 +0400

    drop SubAssignUnchecked / AddAssignUnchecked for storage guard

--- contracts/src/utils/math/storage.rs
+++ contracts/src/utils/math/storage.rs
@@ -1,18 +1,9 @@
 //! Simple math operations missing in `stylus_sdk::storage`.
 use alloy_primitives::Uint;
-use stylus_sdk::storage::{StorageGuardMut, StorageUint};
+use stylus_sdk::storage::StorageUint;
 
 pub(crate) trait AddAssignUnchecked<T> {
     fn add_assign_unchecked(&mut self, rhs: T);
-}
-
-impl<const B: usize, const L: usize> AddAssignUnchecked<Uint<B, L>>
-    for StorageGuardMut<'_, StorageUint<B, L>>
-{
-    fn add_assign_unchecked(&mut self, rhs: Uint<B, L>) {
-        let new_balance = self.get() + rhs;
-        self.set(new_balance);
-    }
 }
 
 impl<const B: usize, const L: usize> AddAssignUnchecked<Uint<B, L>>
@@ -26,15 +17,6 @@ impl<const B: usize, const L: usize> AddAssignUnchecked<Uint<B, L>>
 
 pub(crate) trait SubAssignUnchecked<T> {
     fn sub_assign_unchecked(&mut self, rhs: T);
-}
-
-impl<const B: usize, const L: usize> SubAssignUnchecked<Uint<B, L>>
-    for StorageGuardMut<'_, StorageUint<B, L>>
-{
-    fn sub_assign_unchecked(&mut self, rhs: Uint<B, L>) {
-        let new_balance = self.get() - rhs;
-        self.set(new_balance);
-    }
 }
 
 impl<const B: usize, const L: usize> SubAssignUnchecked<Uint<B, L>>
