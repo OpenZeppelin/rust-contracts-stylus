d58c61e068f16c1340e959dc064b47e2af7b6e6e
0xNeshi <nenad.misic@openzeppelin.com>
2024-12-11 11:04:30 +0100

    test: move _totalSupply closer to other erc20 fields

--- examples/erc20-flashmint/src/constructor.sol
+++ examples/erc20-flashmint/src/constructor.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.8.24;
 contract Erc20FlashMintExample {
     mapping(address => uint256) private _balances;
     mapping(address => mapping(address => uint256)) private _allowances;
-
     uint256 private _totalSupply;
+
     uint256 private _flashFeeAmount;
     address private _flashFeeReceiverAddress;
 
