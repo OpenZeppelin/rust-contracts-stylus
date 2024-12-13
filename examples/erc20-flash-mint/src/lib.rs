f1c0b0d253a4b51633d41fba6a710ea0ef99c3ee
0xNeshi <nenad.misic@openzeppelin.com>
2024-12-13 08:44:40 +0100

    ref: use erc1155supply-like design

--- examples/erc20-flash-mint/src/lib.rs
+++ examples/erc20-flash-mint/src/lib.rs
@@ -4,9 +4,8 @@ extern crate alloc;
 use alloc::vec::Vec;
 
 use alloy_primitives::{Address, U256};
-use openzeppelin_stylus::token::erc20::{
-    extensions::{Erc20FlashMint, IErc3156FlashLender},
-    Erc20,
+use openzeppelin_stylus::token::erc20::extensions::{
+    Erc20FlashMint, IErc3156FlashLender,
 };
 use stylus_sdk::{
     abi::Bytes,
@@ -17,16 +16,14 @@ use stylus_sdk::{
 #[storage]
 struct Erc20FlashMintExample {
     #[borrow]
-    erc20: Erc20,
-    #[borrow]
     erc20_flash_mint: Erc20FlashMint,
 }
 
 #[public]
-#[inherit(Erc20)]
+#[inherit(Erc20FlashMint)]
 impl Erc20FlashMintExample {
     fn max_flash_loan(&self, token: Address) -> U256 {
-        self.erc20_flash_mint.max_flash_loan(token, &self.erc20)
+        self.erc20_flash_mint.max_flash_loan(token)
     }
 
     fn flash_fee(&self, token: Address, amount: U256) -> Result<U256, Vec<u8>> {
@@ -40,16 +37,10 @@ impl Erc20FlashMintExample {
         amount: U256,
         data: Bytes,
     ) -> Result<bool, Vec<u8>> {
-        Ok(self.erc20_flash_mint.flash_loan(
-            receiver,
-            token,
-            amount,
-            data,
-            &mut self.erc20,
-        )?)
+        Ok(self.erc20_flash_mint.flash_loan(receiver, token, amount, data)?)
     }
 
     fn mint(&mut self, to: Address, value: U256) -> Result<(), Vec<u8>> {
-        Ok(self.erc20._mint(to, value)?)
+        Ok(self.erc20_flash_mint._mint(to, value)?)
     }
 }
