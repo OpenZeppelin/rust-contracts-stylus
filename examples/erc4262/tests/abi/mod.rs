6d21c4e8868928f3ef77c8639f318f056e4e7b6f
Angry Coding <mailidanielifechukwu@gmail.com>
2024-12-19 10:34:49 +0000

    chore: erc20 abi

--- examples/erc4262/tests/abi/mod.rs
+++ examples/erc4262/tests/abi/mod.rs
@@ -5,7 +5,15 @@ use alloy::sol;
 sol!(
     #[sol(rpc)]
     contract Erc4262 {
-        
+          function name() external view returns (string name);
+        function symbol() external view returns (string symbol);
+        function decimals() external view returns (uint8 decimals);
+        function totalSupply() external view returns (uint256 totalSupply);
+        function balanceOf(address account) external view returns (uint256 balance);
+        function transfer(address recipient, uint256 amount) external returns (bool);
+        function allowance(address owner, address spender) external view returns (uint256 allowance);
+        function approve(address spender, uint256 amount) external returns (bool);
+        function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
 
         error ERC20InsufficientBalance(address sender, uint256 balance, uint256 needed);
         error ERC20InvalidSender(address sender);
