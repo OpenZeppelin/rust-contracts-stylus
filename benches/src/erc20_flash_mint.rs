93dc548a27281219ece0b1e61dd95ee3c459e327
0xNeshi <nenad.misic@openzeppelin.com>
2024-12-19 10:50:07 +0100

    chore: uncomment borrower deployment

--- benches/src/erc20_flash_mint.rs
+++ benches/src/erc20_flash_mint.rs
@@ -56,21 +56,18 @@ pub async fn run_with(
 
     let contract_addr = deploy(&alice, cache_opt.clone()).await?;
 
-    // TODO: uncomment once it's possible to call ERC20 functions from
-    // within `ERC3156FlashBorrowerMock`.
-    // let receiver_addr = deploy_receiver(&alice, cache_opt).await?;
+    let borrower_addr = deploy_borrower(&alice, cache_opt).await?;
 
     let contract = Erc20FlashMint::new(contract_addr, &alice_wallet);
 
     let amount = uint!(100_U256);
 
-    // IMPORTANT: Order matters!
     use Erc20FlashMint::*;
     #[rustfmt::skip]
     let receipts = vec![
         (maxFlashLoanCall::SIGNATURE, receipt!(contract.maxFlashLoan(contract_addr))?),
         (flashFeeCall::SIGNATURE, receipt!(contract.flashFee(contract_addr, amount))?),
-        // (flashLoanCall::SIGNATURE, receipt!(contract.flashLoan(receiver_addr, contract_addr, amount, vec![].into()))?),
+        (flashLoanCall::SIGNATURE, receipt!(contract.flashLoan(borrower_addr, contract_addr, amount, vec![].into()))?),
     ];
 
     receipts
@@ -91,15 +88,15 @@ async fn deploy(
     crate::deploy(account, "erc20-flash-mint", Some(args), cache_opt).await
 }
 
-// async fn deploy_receiver(
-//     account: &Account,
-//     cache_opt: CacheOpt,
-// ) -> eyre::Result<Address> {
-//     let args = ERC3156FlashBorrowerMock::constructorCall {
-//         enableApprove: true,
-//         enableReturn: true,
-//     };
-//     let args = alloy::hex::encode(args.abi_encode());
-//     crate::deploy(account, "erc3156-flash-borrower-mock", Some(args),
-// cache_opt)         .await
-// }
+async fn deploy_borrower(
+    account: &Account,
+    cache_opt: CacheOpt,
+) -> eyre::Result<Address> {
+    let args = ERC3156FlashBorrowerMock::constructorCall {
+        enableApprove: true,
+        enableReturn: true,
+    };
+    let args = alloy::hex::encode(args.abi_encode());
+    crate::deploy(account, "erc3156-flash-borrower-mock", Some(args), cache_opt)
+        .await
+}
