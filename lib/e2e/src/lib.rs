d7cca492f76ce57b57eff5e710cf0f6b55be9b25
0xNeshi <nenad.misic@openzeppelin.com>
2024-12-20 13:05:21 +0100

    docs: update e2e alloy links

--- lib/e2e/src/lib.rs
+++ lib/e2e/src/lib.rs
@@ -18,7 +18,7 @@ pub use system::{fund_account, provider, Provider, Wallet};
 /// This macro provides a shorthand for broadcasting the transaction to the
 /// network.
 ///
-/// See: <https://alloy-rs.github.io/alloy/alloy_contract/struct.CallBuilder.html>
+/// See: <https://docs.rs/alloy-contract/latest/alloy_contract/struct.CallBuilder.html>
 ///
 /// # Examples
 ///
@@ -43,7 +43,7 @@ macro_rules! send {
 /// This macro provides a shorthand for broadcasting the transaction
 /// to the network, and then waiting for the given number of confirmations.
 ///
-/// See: <https://alloy-rs.github.io/alloy/alloy_provider/heart/struct.PendingTransactionBuilder.html>
+/// See: <https://docs.rs/alloy-provider/latest/alloy_provider/struct.PendingTransactionBuilder.html>
 ///
 /// # Examples
 ///
@@ -69,7 +69,7 @@ macro_rules! watch {
 /// to the network, waiting for the given number of confirmations, and then
 /// fetching the transaction receipt.
 ///
-/// See: <https://alloy-rs.github.io/alloy/alloy_provider/heart/struct.PendingTransactionBuilder.html>
+/// See: <https://docs.rs/alloy-provider/latest/alloy_provider/struct.PendingTransactionBuilder.html>
 ///
 /// # Examples
 ///
