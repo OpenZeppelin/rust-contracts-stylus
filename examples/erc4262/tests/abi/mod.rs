fea2fb104f223aa2482d2448d04cf7f2bfa03a44
Angry Coding <mailidanielifechukwu@gmail.com>
2024-12-23 07:54:04 +0000

    chore:  test abi  for erc4262

--- examples/erc4262/tests/abi/mod.rs
+++ examples/erc4262/tests/abi/mod.rs
@@ -5,7 +5,7 @@ use alloy::sol;
 sol!(
     #[sol(rpc)]
     contract Erc4262 {
-          function name() external view returns (string name);
+        function name() external view returns (string name);
         function symbol() external view returns (string symbol);
         function decimals() external view returns (uint8 decimals);
         function totalSupply() external view returns (uint256 totalSupply);
@@ -14,6 +14,23 @@ sol!(
         function allowance(address owner, address spender) external view returns (uint256 allowance);
         function approve(address spender, uint256 amount) external returns (bool);
         function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+        function asset() public view  returns (address);
+        function totalAssets() public view returns (uint256); 
+        function convertToShares(uint256 assets) public view  returns (uint256); 
+        function convertToAssets(uint256 shares) public view  returns (uint256);
+        function maxMint(address) public view  returns (uint256);
+        function maxDeposit(address) public view  returns (uint256);
+        function maxWithdraw(address owner) public view  returns (uint256);
+        function maxRedeem(address owner) public view  returns (uint256);
+        function previewDeposit(uint256 assets) public view  returns (uint256); 
+        function previewMint(uint256 shares) public view  returns (uint256);
+        function previewRedeem(uint256 shares) public view  returns (uint256);
+        function previewWithdraw(uint256 assets) public view  returns (uint256);
+        function deposit(uint256 assets, address receiver) public  returns (uint256);
+        function mint(uint256 shares, address receiver) public  returns (uint256);
+        function redeem(uint256 shares, address receiver) public  returns (uint256);
+        function withdraw(uint256 assets, address receiver) public  returns (uint256);
 
         error ERC20InsufficientBalance(address sender, uint256 balance, uint256 needed);
         error ERC20InvalidSender(address sender);
@@ -31,5 +48,11 @@ sol!(
         event Transfer(address indexed from, address indexed to, uint256 value);
         #[derive(Debug, PartialEq)]
         event Approval(address indexed owner, address indexed spender, uint256 value);
+
+        #[allow(missing_docs)]
+        event Deposit(address indexed sender, address indexed owner, uint256 assets, uint256 shares);
+
+        #[allow(missing_docs)]
+        event Withdraw(address indexed sender,address indexed receiver,ddress indexed owner,uint256 assets, uint256 shares);
     }
 );
