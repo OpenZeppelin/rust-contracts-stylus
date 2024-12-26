26db2e0cc5ea9bd8bec90243858106d82e3d0954
0xNeshi <nenad.misic@openzeppelin.com>
2024-12-26 08:33:18 +0100

    ref: revert morsu to 0.2.1

--- fuzz/src/to_typed_data_hash.rs
+++ fuzz/src/to_typed_data_hash.rs
@@ -1,7 +1,8 @@
 use openzeppelin_stylus::utils::cryptography::eip712::to_typed_data_hash;
 
-#[motsu::fuzz]
-fn to_typed_data_hash(data: ([u8; 32], [u8; 32])) {
-    let (domain_separator, struct_hash) = data;
-    let _ = to_typed_data_hash(&domain_separator, &struct_hash);
+fn main() {
+    afl::fuzz!(|data: ([u8; 32], [u8; 32])| {
+        let (domain_separator, struct_hash) = data;
+        let _ = to_typed_data_hash(&domain_separator, &struct_hash);
+    })
 }
