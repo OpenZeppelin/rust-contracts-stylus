da89ce498ad48c3f85a9d91f73485231879fd0f7
Nenad <nenad.misic@openzeppelin.com>
2024-12-26 20:19:16 +0100

    test: update comments in e2e-tests.sh

--- scripts/e2e-tests.sh
+++ scripts/e2e-tests.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 set -e
 
-# Move to project root
+# Navigate to project root
 cd "$(dirname "$(realpath "$0")")/.."
 
-# Build the project
 cargo build --release --target wasm32-unknown-unknown -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort
 
-# Set RPC_URL environment variable
 export RPC_URL=http://localhost:8547
 
-# Run tests based on arguments
+# If any arguments are set, just pass them as-is to the cargo test command
 if [[ $# -eq 0 ]]; then
     cargo test --features std,e2e --test "*"
 else
