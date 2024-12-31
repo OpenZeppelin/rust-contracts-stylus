8a9ef13a61e889ed60dc4819d05bcdccc630c4d4
0xNeshi <nenad.misic@openzeppelin.com>
2024-12-31 08:30:24 +0100

    test: remove dir navigation in e2e-tests.sh

--- scripts/e2e-tests.sh
+++ scripts/e2e-tests.sh
@@ -3,10 +3,6 @@ set -e
 
 TEST_ARG="${1:-*}"
 
-MYDIR=$(realpath "$(dirname "$0")")
-cd "$MYDIR"
-cd ..
-
 cargo build --release --target wasm32-unknown-unknown -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort
 
 export RPC_URL=http://localhost:8547
