0e200ce616875f10ea2b4241a3946f9d435cdd7f
Alisander Qoshqosh <qalisander@gmail.com>
2025-01-08 09:13:19 +0400

    ++

--- examples/poseidon/src/lib.rs
+++ examples/poseidon/src/lib.rs
@@ -6,8 +6,8 @@ use alloc::vec::Vec;
 use alloy_primitives::U256;
 use openzeppelin_crypto::{
     bigint::{crypto_bigint::Uint, BigInteger},
-    field::{instance::FpVesta, prime::PrimeField},
-    poseidon2::{instance::vesta::VestaParams, Poseidon2},
+    field::{instance::FpBN256, prime::PrimeField},
+    poseidon2::{instance::bn256::BN256Params, Poseidon2},
 };
 use stylus_sdk::prelude::{entrypoint, public, storage};
 
@@ -18,10 +18,10 @@ struct PoseidonExample {}
 #[public]
 impl PoseidonExample {
     pub fn hash(&mut self, inputs: [U256; 2]) -> Result<U256, Vec<u8>> {
-        let mut hasher = Poseidon2::<VestaParams, FpVesta>::new();
+        let mut hasher = Poseidon2::<BN256Params, FpBN256>::new();
 
         for input in inputs.iter() {
-            let fp = FpVesta::from_bigint(Uint::from_bytes_le(
+            let fp = FpBN256::from_bigint(Uint::from_bytes_le(
                 &input.to_le_bytes_vec(),
             ));
             hasher.absorb(&fp);
