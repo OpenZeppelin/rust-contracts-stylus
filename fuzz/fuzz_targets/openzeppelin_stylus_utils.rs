de60bf8bd5d10b6efe9e4a5d77379688be16aeff
0xNeshi <nenad.misic@openzeppelin.com>
2025-01-09 11:58:16 +0100

    test: add openzeppelin_stylus::utils fuzz test (broken)

---
+++ fuzz/fuzz_targets/openzeppelin_stylus_utils.rs
@@ -0,0 +1,27 @@
+#![no_main]
+#[macro_use]
+extern crate libfuzzer_sys;
+extern crate openzeppelin_stylus;
+
+use alloy_primitives::B256;
+pub use motsu::prelude::*;
+
+struct Storage;
+
+unsafe impl stylus_sdk::storage::TopLevelStorage for Storage {}
+
+fuzz_target!(|data: (B256, u8, B256, B256)| {
+    let (hash, v, r, s) = data;
+
+    let mut storage = Storage;
+
+    let _ = openzeppelin_stylus::utils::cryptography::ecdsa::recover(
+        &mut storage,
+        hash,
+        v,
+        r,
+        s,
+    );
+});
+
+// add missing shims
