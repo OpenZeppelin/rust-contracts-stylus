e8248beb81288169cc17861960a5eef1121b0a6b
0xNeshi <nenad.misic@openzeppelin.com>
2025-01-09 10:54:19 +0100

    ref: remove comments from e2e script

--- scripts/e2e-tests.sh
+++ scripts/e2e-tests.sh
@@ -4,10 +4,8 @@ set -e
 # Navigate to project root
 cd "$(dirname "$(realpath "$0")")/.."
 
-# Build the project
 cargo build --release --target wasm32-unknown-unknown -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort
 
-# Set RPC_URL environment variable
 export RPC_URL=http://localhost:8547
 
 # If any arguments are set, just pass them as-is to the cargo test command
