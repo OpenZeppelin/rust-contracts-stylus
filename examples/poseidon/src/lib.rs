38df426d4a7a13e9a2536b4950d7d09877f0723d
Alisander Qoshqosh <qalisander@gmail.com>
2025-01-11 03:57:26 +0400

    biginteger ff multiplication benchmark

--- examples/poseidon/src/lib.rs
+++ examples/poseidon/src/lib.rs
@@ -6,7 +6,9 @@ use alloc::vec::Vec;
 use alloy_primitives::U256;
 use openzeppelin_crypto::{
     arithmetic::{BigInt, BigInteger},
-    field::{instance::FpBN256, prime::PrimeField},
+    field::{
+        group::AdditiveGroup, instance::FpBN256, prime::PrimeField, Field,
+    },
     poseidon2::{instance::bn256::BN256Params, Poseidon2},
 };
 use stylus_sdk::prelude::{entrypoint, public, storage};
@@ -18,18 +20,25 @@ struct PoseidonExample {}
 #[public]
 impl PoseidonExample {
     pub fn hash(&mut self, inputs: [U256; 2]) -> Result<U256, Vec<u8>> {
-        let mut hasher = Poseidon2::<BN256Params, FpBN256>::new();
+        let inputs: Vec<_> = inputs
+            .iter()
+            .map(|input| {
+                FpBN256::from_bigint(BigInt::from_bytes_le(
+                    &input.to_le_bytes_vec(),
+                ))
+            })
+            .collect();
 
-        for input in inputs.iter() {
-            let fp = FpBN256::from_bigint(BigInt::from_bytes_le(
-                &input.to_le_bytes_vec(),
-            ));
-            hasher.absorb(&fp);
+        let mut res = FpBN256::ONE;
+        for _ in 0..1000 {
+            for input in inputs.iter() {
+                res *= input;
+                res.square_in_place();
+            }
         }
 
-        let hash = hasher.squeeze();
-        let hash = hash.into_bigint().into_bytes_le();
+        let res = res.into_bigint().into_bytes_le();
 
-        Ok(U256::from_le_slice(&hash))
+        Ok(U256::from_le_slice(&res))
     }
 }
