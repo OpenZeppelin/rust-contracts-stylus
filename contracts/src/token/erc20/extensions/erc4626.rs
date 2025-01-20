47d71724f6918c70e7cc8fa1c1241737b79c2c7b
Daniel Bigos <daniel.bigos@icloud.com>
2025-01-20 11:44:13 +0100

    fix: cargo doc

--- contracts/src/token/erc20/extensions/erc4626.rs
+++ contracts/src/token/erc20/extensions/erc4626.rs
@@ -1,45 +1,11 @@
-//! ERC-4626 Tokenized Vault Standard Implementation.
+//! ERC-4626 Tokenized Vault Standard Implementation as defined in [ERC-4626].
 //!
-//! Extends ERC-20 for vaults, enabling minting and burning of "shares"
-//! (represented using the [`ERC-20`] inheritance) in exchange for underlying
-//! assets. This contract provides standardized workflows for deposits, minting,
-//! redemption, and burning of assets. Note: The shares are minted and burned in
-//! relation to the assets via the `deposit`, `mint`, `redeem`, and `burn`
-//! methods, affecting only the shares token, not the asset token itself.
-//!
-//! [ERC]: https://eips.ethereum.org/EIPS/eip-4626
-//!
-//! [CAUTION]
-//! In empty (or nearly empty) ERC-4626 vaults, deposits are at high risk of
-//! being stolen through frontrunning with a "donation" to the vault that
-//! inflates the price of a share. This is variously known as a donation or
-//! inflation attack and is essentially a problem of slippage. Vault deployers
-//! can protect against this attack by making an initial deposit of a
-//! non-trivial amount of the asset, such that price manipulation becomes
-//! infeasible. Withdrawals may similarly be affected by slippage. Users can
-//! protect against this attack as well as unexpected slippage in general by
-//! verifying the amount received is as expected, using a wrapper that performs
-//! these checks such as https://github.com/ERC4626-Alliance/ERC4626-Contracts#erc4626router-and-base
-//!
-//! The `_decimalsOffset()` corresponds to an offset in the decimal
-//! representation between the underlying asset's decimals and the vault
-//! decimals. This offset also determines the rate of virtual shares to virtual
-//! assets in the vault, which itself determines the initial exchange rate.
-//! While not fully preventing the attack, analysis shows that the default
-//! offset (0) makes it non-profitable even if an attacker is able to capture
-//! value from multiple user deposits, as a result of the value being captured
-//! by the virtual shares (out of the attacker's donation) matching the
-//! attacker's expected gains. With a larger offset, the attack becomes orders
-//! of magnitude more expensive than it is profitable. More details about the
-//! underlying math can be found xref:erc4626.adoc#inflation-attack[here].
-//!
-//! The drawback of this approach is that the virtual shares do capture (a very
-//! small) part of the value being accrued to the vault. Also, if the vault
-//! experiences losses, the users try to exit the vault, the virtual shares and
-//! assets will cause the first user to exit to experience reduced losses in
-//! detriment to the last users that will experience bigger losses.
-//!
-//! To learn more, check out our xref:ROOT:erc4626.adoc[ERC-4626 guide]..
+//! This extension allows the minting and burning of "shares" (represented using
+//! the ERC-20 inheritance) in exchange for underlying "assets" through
+//! standardized `deposit`, `mint`, `redeem` and `burn` workflows. This contract
+//! extends the ERC-20 standard. Any additional extensions included along it
+//! would affect the "shares" token represented by this contract and not the
+//! "assets" token which is an independent contract.
 
 use alloy_primitives::{Address, U256};
 use alloy_sol_macro::sol;
