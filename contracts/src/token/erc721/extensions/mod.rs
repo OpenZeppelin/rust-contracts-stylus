7281605a06897a96d8f0c6b058aeb39b1f144eb9
18aaddy <aaddyrocks123@gmail.com>
2025-01-23 19:43:03 +0530

    update: added documentation

--- contracts/src/token/erc721/extensions/mod.rs
+++ contracts/src/token/erc721/extensions/mod.rs
@@ -3,8 +3,8 @@ pub mod burnable;
 pub mod consecutive;
 pub mod enumerable;
 pub mod metadata;
-pub mod uri_storage;
 pub mod royalty;
+pub mod uri_storage;
 
 pub use burnable::IErc721Burnable;
 pub use enumerable::{Erc721Enumerable, IErc721Enumerable};
