1bedc1691fed13a7d72a76a5d3f76f606c78b8d2
Angry Coding <mailidanielifechukwu@gmail.com>
2025-01-26 23:46:03 +0000

    chore: erc20 wrapper  example

--- examples/erc20-wrapper/src/lib.rs
+++ examples/erc20-wrapper/src/lib.rs
@@ -3,13 +3,11 @@ extern crate alloc;
 
 use alloc::vec::Vec;
 
-use openzeppelin_stylus::token::{
-    erc20::{
-        extensions::{Erc20Metadata, Erc20Wrapper},
+use alloy_primitives::{Address, U256};
+use openzeppelin_stylus::token::erc20::{
+        extensions::{Erc20Metadata, Erc20Wrapper,IERC20Wrapper},
         Erc20,
-    },
-    erc721::extensions::Erc721Metadata,
-};
+    };
 use stylus_sdk::prelude::{entrypoint, public, storage};
 
 #[entrypoint]
@@ -24,96 +22,25 @@ struct Erc20WrapperExample {
 }
 
 #[public]
-#[inherit(Erc20, Erc721Metadata)]
+#[inherit(Erc20, Erc20Metadata)]
 impl Erc20WrapperExample {
-    // Overrides the default [`Metadata::decimals`], and sets it to `10`.
-    //
-    // If you don't provide this method in the `entrypoint` contract, it will
-    // default to `18`.
-    // pub fn decimals(&self) -> u8 {
-    //     DECIMALS
-    // }
+    fn underlying(&self) -> Address {
+         self.wrapper.underlying()
+    }
 
-    // pub fn burn(&mut self, value: U256) -> Result<(), Vec<u8>> {
-    //     self.pausable.when_not_paused()?;
-    //     self.erc20.burn(value).map_err(|e| e.into())
-    // }
+    fn deposit_to(
+        &mut self,
+        account: Address,
+        value: U256
+    ) -> Result<bool, Vec<u8>> {
+        Ok(self.wrapper.deposit_to(account, value, &mut self.erc20)?)
+    }
 
-    // pub fn burn_from(
-    //     &mut self,
-    //     account: Address,
-    //     value: U256,
-    // ) -> Result<(), Vec<u8>> {
-    //     self.pausable.when_not_paused()?;
-    //     self.erc20.burn_from(account, value).map_err(|e| e.into())
-    // }
-
-    // // Add token minting feature.
-    // //
-    // // Make sure to handle `Capped` properly. You should not call
-    // // [`Erc20::_update`] to mint tokens -- it will the break `Capped`
-    // // mechanism.
-    // pub fn mint(
-    //     &mut self,
-    //     account: Address,
-    //     value: U256,
-    // ) -> Result<(), Vec<u8>> {
-    //     self.pausable.when_not_paused()?;
-    //     let max_supply = self.capped.cap();
-
-    //     // Overflow check required.
-    //     let supply = self
-    //         .erc20
-    //         .total_supply()
-    //         .checked_add(value)
-    //         .expect("new supply should not exceed `U256::MAX`");
-
-    //     if supply > max_supply {
-    //         return Err(capped::Error::ExceededCap(
-    //             capped::ERC20ExceededCap {
-    //                 increased_supply: supply,
-    //                 cap: max_supply,
-    //             },
-    //         ))?;
-    //     }
-
-    //     self.erc20._mint(account, value)?;
-    //     Ok(())
-    // }
-
-    // pub fn transfer(
-    //     &mut self,
-    //     to: Address,
-    //     value: U256,
-    // ) -> Result<bool, Vec<u8>> {
-    //     self.pausable.when_not_paused()?;
-    //     self.erc20.transfer(to, value).map_err(|e| e.into())
-    // }
-
-    // pub fn transfer_from(
-    //     &mut self,
-    //     from: Address,
-    //     to: Address,
-    //     value: U256,
-    // ) -> Result<bool, Vec<u8>> {
-    //     self.pausable.when_not_paused()?;
-    //     self.erc20.transfer_from(from, to, value).map_err(|e| e.into())
-    // }
-
-    // fn supports_interface(interface_id: FixedBytes<4>) -> bool {
-    //     Erc20::supports_interface(interface_id)
-    //         || Erc20Metadata::supports_interface(interface_id)
-    // }
-
-    // /// WARNING: These functions are intended for **testing purposes** only.
-    // In /// **production**, ensure strict access control to prevent
-    // unauthorized /// pausing or unpausing, which can disrupt contract
-    // functionality. Remove /// or secure these functions before
-    // deployment. pub fn pause(&mut self) -> Result<(), Vec<u8>> {
-    //     self.pausable.pause().map_err(|e| e.into())
-    // }
-
-    // pub fn unpause(&mut self) -> Result<(), Vec<u8>> {
-    //     self.pausable.unpause().map_err(|e| e.into())
-    // }
+    fn withdraw_to(
+        &mut self,
+        account: Address,
+        value: U256
+    ) -> Result<bool, Vec<u8>> {
+        Ok(self.wrapper.withdraw_to(account, value, &mut self.erc20)?)
+    }
 }
