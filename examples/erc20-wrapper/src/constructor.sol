3405bf2086c6fffc605e85e7d4aa2130eb502af7
Angry Coding <mailidanielifechukwu@gmail.com>
2025-01-27 01:11:51 +0000

    chore: fomrmat

--- examples/erc20-wrapper/src/constructor.sol
+++ examples/erc20-wrapper/src/constructor.sol
@@ -2,14 +2,22 @@
 pragma solidity ^0.8.21;
 
 contract Erc20WrapperExample {
+
+    
     mapping(address account => uint256) private _balances;
     mapping(address account => mapping(address spender => uint256))
         private _allowances;
     uint256 private _totalSupply;
     string private _name;
     string private _symbol;
-    constructor(string memory name_, string memory symbol_) {
+
+
+    address private _underlyingToken;
+
+
+    constructor(string memory name_, string memory symbol_, address underlyingToken_) {
         _name = name_;
         _symbol = symbol_;
+        _underlyingToken = underlyingToken_;
     }
 }
--- examples/erc20-wrapper/tests/erc20wrapper.rs
+++ examples/erc20-wrapper/tests/erc20wrapper.rs
@@ -14,23 +14,18 @@ use eyre::Result;
 use crate::Erc20WrapperExample::constructorCall;
 
 mod abi;
+mod mock;
+
+use mock::{erc20, erc20::ERC20Mock};
 
 sol!("src/constructor.sol");
-
-const TOKEN_NAME: &str = "Test Token";
-const TOKEN_SYMBOL: &str = "TTK";
 
 const WRAPPED_TOKEN_NAME: &str = "WRAPPED Test Token";
 const WRAPPED_TOKEN_SYMBOL: &str = "WTTK";
 
-impl Default for constructorCall {
-    fn default() -> Self {
-        ctr()
-    }
-}
-
-fn ctr() -> constructorCall {
+fn ctr(asset: Address) -> constructorCall {
     Erc20WrapperExample::constructorCall {
+        underlyingToken_: asset,
         name_: WRAPPED_TOKEN_NAME.to_owned(),
         symbol_: WRAPPED_TOKEN_SYMBOL.to_owned(),
     }
@@ -42,9 +37,10 @@ fn ctr() -> constructorCall {
 
 #[e2e::test]
 async fn constructs(alice: Account) -> Result<()> {
+    let asset_address = erc20::deploy(&alice.wallet).await?;
     let contract_addr = alice
         .as_deployer()
-        .with_default_constructor::<constructorCall>()
+        .with_constructor(ctr(asset_address))
         .deploy()
         .await?
         .address()?;
@@ -53,7 +49,7 @@ async fn constructs(alice: Account) -> Result<()> {
     let symbol = contract.symbol().call().await?.symbol;
     let decimals = contract.decimals().call().await?.decimals;
 
-    assert_eq!(name, WRAPPED_TOKEN_NAME.to_owned());     
+    assert_eq!(name, WRAPPED_TOKEN_NAME.to_owned());
     assert_eq!(symbol, WRAPPED_TOKEN_SYMBOL.to_owned());
     assert_eq!(decimals, 18);
 
