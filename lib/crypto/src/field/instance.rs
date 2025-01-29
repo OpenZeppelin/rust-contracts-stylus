68e6a51efcf97ae2f5ea24adc9a3beaba5456d8b
Alisander Qoshqosh <qalisander@gmail.com>
2025-01-29 11:26:45 +0400

    ++

--- lib/crypto/src/field/instance.rs
+++ lib/crypto/src/field/instance.rs
@@ -3,7 +3,7 @@
 #![allow(missing_docs)]
 
 use crate::{
-    arithmetic::uint::Uint,
+    arithmetic::uint::{U256, U64},
     field::fp::{Fp256, Fp64, FpParams, LIMBS_256, LIMBS_64},
     fp_from_num, from_num,
 };
@@ -12,40 +12,40 @@ pub type FpVesta = Fp256<VestaParam>;
 pub struct VestaParam;
 impl FpParams<LIMBS_256> for VestaParam {
     const GENERATOR: Fp256<VestaParam> = fp_from_num!("5");
-    const MODULUS: Uint<LIMBS_256> = from_num!("28948022309329048855892746252171976963363056481941647379679742748393362948097");
+    const MODULUS: U256 = from_num!("28948022309329048855892746252171976963363056481941647379679742748393362948097");
 }
 
 pub type FpBabyBear = Fp64<BabyBearParam>;
 pub struct BabyBearParam;
 impl FpParams<LIMBS_64> for BabyBearParam {
     const GENERATOR: Fp64<BabyBearParam> = fp_from_num!("31");
-    const MODULUS: Uint<LIMBS_64> = from_num!("2013265921");
+    const MODULUS: U64 = from_num!("2013265921");
 }
 
 pub type FpBLS12 = Fp256<BLS12Param>;
 pub struct BLS12Param;
 impl FpParams<LIMBS_256> for BLS12Param {
     const GENERATOR: Fp256<BLS12Param> = fp_from_num!("7");
-    const MODULUS: Uint<LIMBS_256> = from_num!("52435875175126190479447740508185965837690552500527637822603658699938581184513");
+    const MODULUS: U256 = from_num!("52435875175126190479447740508185965837690552500527637822603658699938581184513");
 }
 
 pub type FpBN256 = Fp256<BN256Param>;
 pub struct BN256Param;
 impl FpParams<LIMBS_256> for BN256Param {
     const GENERATOR: Fp256<BN256Param> = fp_from_num!("7");
-    const MODULUS: Uint<LIMBS_256> = from_num!("21888242871839275222246405745257275088548364400416034343698204186575808495617");
+    const MODULUS: U256 = from_num!("21888242871839275222246405745257275088548364400416034343698204186575808495617");
 }
 
 pub type FpGoldiLocks = Fp64<GoldiLocksParam>;
 pub struct GoldiLocksParam;
 impl FpParams<LIMBS_64> for GoldiLocksParam {
     const GENERATOR: Fp64<GoldiLocksParam> = fp_from_num!("7");
-    const MODULUS: Uint<LIMBS_64> = from_num!("18446744069414584321");
+    const MODULUS: U64 = from_num!("18446744069414584321");
 }
 
 pub type FpPallas = Fp256<PallasParam>;
 pub struct PallasParam;
 impl FpParams<LIMBS_256> for PallasParam {
     const GENERATOR: Fp256<PallasParam> = fp_from_num!("5");
-    const MODULUS: Uint<LIMBS_256> = from_num!("28948022309329048855892746252171976963363056481941560715954676764349967630337");
+    const MODULUS: U256 = from_num!("28948022309329048855892746252171976963363056481941560715954676764349967630337");
 }
