1c86069c6ce2fee555361c74f7aa6bd1063f7440
Nenad <nenad.misic@openzeppelin.com>
2025-02-05 18:30:55 +0100

    test: Investigate Fuzzing (#477)
    <!--
    Thank you for your interest in contributing to OpenZeppelin!
    
    Consider opening an issue for discussion prior to submitting a PR. New
    features will be merged faster if they were first discussed and designed
    with the team.
    
    Describe the changes introduced in this pull request. Include any
    context necessary for understanding the PR's purpose.
    -->
    
    As [per our
    agreement](https://openzeppelin.slack.com/archives/C06R5AGFE9G/p1737021738986409),
    this PR includes a basic fuzz tests setup and some missing unit/property
    tests for `openzeppelin-crypto` crate.
    
    I created a couple of related issues to address missing property tests
    (see #501 #502 #503).
    Adding property tests for contract will be easily possible after `motsu`
    is refactored (see
    https://github.com/OpenZeppelin/stylus-test-helpers/pull/14/files#r1919897820)
    
    <!-- Fill in with issue number -->
    Resolves #458 
    
    #### PR Checklist
    
    <!--
    Before merging the pull request all of the following must be completed.
    Feel free to submit a PR or Draft PR even if some items are pending.
    Some of the items may not apply.
    -->
    
    - [x] Tests
    - [x] Documentation
    - [ ] Changelog
    
    ---------
    
    Co-authored-by: Daniel Bigos <daniel.bigos@openzeppelin.com>
    Co-authored-by: Alisander Qoshqosh <37006439+qalisander@users.noreply.github.com>

--- lib/crypto/src/bits.rs
+++ lib/crypto/src/bits.rs
@@ -32,14 +32,68 @@ impl_bit_iter_be!(usize);
 
 #[cfg(all(test, feature = "std"))]
 mod tests {
+    use num_traits::ConstOne;
+    use proptest::prelude::*;
+
     use super::*;
 
     #[test]
-    fn u64_bit_iterator_be() {
-        let num: u64 = 0b1100;
-        let bits: Vec<bool> = num.bit_be_trimmed_iter().collect();
+    fn known_pattern() {
+        let value = 0b1100u64;
+        let full = value.bit_be_iter().collect::<Vec<_>>();
+        let expected = [false; 60]
+            .iter()
+            .chain(&[true, true, false, false])
+            .copied()
+            .collect::<Vec<_>>();
+        assert_eq!(full, expected);
 
-        assert_eq!(bits.len(), 4);
-        assert_eq!(bits, vec![true, true, false, false]);
+        let trimmed = value.bit_be_trimmed_iter().collect::<Vec<_>>();
+        let expected = vec![true, true, false, false];
+        assert_eq!(trimmed, expected);
+    }
+
+    macro_rules! trimmed_is_subset_of_full {
+        ($ty:ident) => {{
+            proptest!(|(value: $ty)| {
+                let full: Vec<bool> = value.bit_be_iter().collect();
+                let trimmed: Vec<bool> = value.bit_be_trimmed_iter().collect();
+                let start_idx = value.leading_zeros() as usize;
+                prop_assert_eq!(&full[start_idx..], trimmed);
+            });
+        }};
+    }
+
+    #[test]
+    fn trimmed_is_subset_of_full() {
+        trimmed_is_subset_of_full!(u8);
+        trimmed_is_subset_of_full!(u16);
+        trimmed_is_subset_of_full!(u32);
+        trimmed_is_subset_of_full!(u64);
+        trimmed_is_subset_of_full!(u128);
+        trimmed_is_subset_of_full!(usize);
+    }
+
+    macro_rules! edge_case {
+        ($ty:ident) => {{
+            assert_eq!($ty::MIN.bit_be_trimmed_iter().count(), 0);
+            assert_eq!($ty::MIN.bit_be_iter().count(), $ty::BITS as usize);
+            assert_eq!(
+                $ty::MAX.bit_be_trimmed_iter().count(),
+                $ty::BITS as usize
+            );
+            assert_eq!($ty::MAX.bit_be_iter().count(), $ty::BITS as usize);
+            assert_eq!($ty::ONE.bit_be_trimmed_iter().count(), usize::ONE);
+        }};
+    }
+
+    #[test]
+    fn edge_cases() {
+        edge_case!(u8);
+        edge_case!(u16);
+        edge_case!(u32);
+        edge_case!(u64);
+        edge_case!(u128);
+        edge_case!(usize);
     }
 }
