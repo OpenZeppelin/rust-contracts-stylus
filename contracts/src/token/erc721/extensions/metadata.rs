4b6a35d12374a71b7c34e68cce6f37fd6536e45c
Nenad <nenad.misic@openzeppelin.com>
2025-02-06 17:17:10 +0100

    feat: erc721consecutive ctor (broken)

--- contracts/src/token/erc721/extensions/metadata.rs
+++ contracts/src/token/erc721/extensions/metadata.rs
@@ -59,6 +59,16 @@ impl IErc721Metadata for Erc721Metadata {
     }
 }
 
+// TODO: uncomment once multiple public attributes are supported
+// #[public]
+impl Erc721Metadata {
+    /// Constructor
+    // #[constructor]
+    pub fn constructor(&mut self, name: String, symbol: String) {
+        self._metadata.constructor(name, symbol);
+    }
+}
+
 const TOKEN_URI_SELECTOR: u32 =
     u32::from_be_bytes(stylus_sdk::function_selector!("tokenURI", U256));
 
