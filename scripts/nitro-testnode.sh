048475fc1110de62f4aac39091ae414fe5d18817
Daniel Bigos <daniel.bigos@icloud.com>
2025-02-11 16:51:37 +0100

    build: bump to fix commit

--- scripts/nitro-testnode.sh
+++ scripts/nitro-testnode.sh
@@ -51,7 +51,7 @@ then
   git clone --recurse-submodules https://github.com/OffchainLabs/nitro-testnode.git
   cd ./nitro-testnode || exit
   git pull origin release --recurse-submodules
-  git checkout 1a01f930340e93cd06953dd7b05f036314715aa4 || exit
+  git checkout e189155bdcdb5deb1ec6d2df7b73a7d2878943a3 || exit
 
   ./test-node.bash --no-run --init || exit
 fi
