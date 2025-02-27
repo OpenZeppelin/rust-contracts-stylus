99ee75602bcd8cf2bcb3e7e1085428a51c47e96b
0xNeshi <nenad.misic@openzeppelin.com>
2025-02-27 09:52:15 +0100

    feat: remove IERC165 impls from Erc20 contracts

--- examples/erc20/src/lib.rs
+++ examples/erc20/src/lib.rs
@@ -3,13 +3,13 @@ extern crate alloc;
 
 use alloc::vec::Vec;
 
-use alloy_primitives::{Address, FixedBytes, U256};
+use alloy_primitives::{Address, U256};
 use openzeppelin_stylus::{
     token::erc20::{
         extensions::{capped, Capped, Erc20Metadata, IErc20Burnable},
         Erc20, IErc20,
     },
-    utils::{introspection::erc165::IErc165, Pausable},
+    utils::Pausable,
 };
 use stylus_sdk::prelude::{entrypoint, public, storage};
 
@@ -103,11 +103,6 @@ impl Erc20Example {
     ) -> Result<bool, Vec<u8>> {
         self.pausable.when_not_paused()?;
         self.erc20.transfer_from(from, to, value).map_err(|e| e.into())
-    }
-
-    fn supports_interface(interface_id: FixedBytes<4>) -> bool {
-        Erc20::supports_interface(interface_id)
-            || Erc20Metadata::supports_interface(interface_id)
     }
 
     /// WARNING: These functions are intended for **testing purposes** only. In
