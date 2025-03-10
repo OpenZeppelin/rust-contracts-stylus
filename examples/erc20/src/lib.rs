7ba39aad5a47d86dc5048710880b9f0c0d9bc9e8
Nenad <nenad.misic@openzeppelin.com>
2025-03-10 16:52:54 +0100

    ref: remove redundant 'pub' from examples & docs

--- examples/erc20/src/lib.rs
+++ examples/erc20/src/lib.rs
@@ -1,124 +1,17 @@
-#![cfg_attr(not(test), no_main)]
-extern crate alloc;
-
-use alloc::vec::Vec;
-
-use alloy_primitives::{Address, FixedBytes, U256};
-use openzeppelin_stylus::{
-    token::erc20::{
-        self,
-        extensions::{capped, Capped, Erc20Metadata, IErc20Burnable},
-        Erc20, IErc20,
-    },
-    utils::{introspection::erc165::IErc165, pausable, Pausable},
-};
-use stylus_sdk::prelude::*;
-
-const DECIMALS: u8 = 10;
-
-#[derive(SolidityError, Debug)]
-enum Error {
-    Capped(capped::Error),
-    Erc20(erc20::Error),
-    Pausable(pausable::Error),
-}
-
-#[entrypoint]
-#[storage]
-struct Erc20Example {
-    #[borrow]
-    erc20: Erc20,
-    #[borrow]
-    metadata: Erc20Metadata,
-    #[borrow]
-    capped: Capped,
-    #[borrow]
-    pausable: Pausable,
-}
-
 #[public]
-#[inherit(Erc20, Erc20Metadata, Capped, Pausable)]
+#[inherit(Erc20)]
 impl Erc20Example {
-    // Overrides the default [`Metadata::decimals`], and sets it to `10`.
-    //
-    // If you don't provide this method in the `entrypoint` contract, it will
-    // default to `18`.
-    fn decimals(&self) -> u8 {
-        DECIMALS
-    }
-
-    fn burn(&mut self, value: U256) -> Result<(), Error> {
-        self.pausable.when_not_paused()?;
-        self.erc20.burn(value).map_err(|e| e.into())
+    fn burn(&mut self, value: U256) -> Result<(), erc20::Error> {
+        // ...
+        self.erc20.burn(value)
     }
 
     fn burn_from(
         &mut self,
         account: Address,
         value: U256,
-    ) -> Result<(), Error> {
-        self.pausable.when_not_paused()?;
-        self.erc20.burn_from(account, value).map_err(|e| e.into())
-    }
-
-    // Add token minting feature.
-    //
-    // Make sure to handle `Capped` properly. You should not call
-    // [`Erc20::_update`] to mint tokens -- it will the break `Capped`
-    // mechanism.
-    fn mint(&mut self, account: Address, value: U256) -> Result<(), Error> {
-        self.pausable.when_not_paused()?;
-        let max_supply = self.capped.cap();
-
-        // Overflow check required.
-        let supply = self
-            .erc20
-            .total_supply()
-            .checked_add(value)
-            .expect("new supply should not exceed `U256::MAX`");
-
-        if supply > max_supply {
-            return Err(capped::Error::ExceededCap(
-                capped::ERC20ExceededCap {
-                    increased_supply: supply,
-                    cap: max_supply,
-                },
-            ))?;
-        }
-
-        self.erc20._mint(account, value)?;
-        Ok(())
-    }
-
-    fn transfer(&mut self, to: Address, value: U256) -> Result<bool, Error> {
-        self.pausable.when_not_paused()?;
-        self.erc20.transfer(to, value).map_err(|e| e.into())
-    }
-
-    fn transfer_from(
-        &mut self,
-        from: Address,
-        to: Address,
-        value: U256,
-    ) -> Result<bool, Error> {
-        self.pausable.when_not_paused()?;
-        self.erc20.transfer_from(from, to, value).map_err(|e| e.into())
-    }
-
-    fn supports_interface(interface_id: FixedBytes<4>) -> bool {
-        Erc20::supports_interface(interface_id)
-            || Erc20Metadata::supports_interface(interface_id)
-    }
-
-    /// WARNING: These functions are intended for **testing purposes** only. In
-    /// **production**, ensure strict access control to prevent unauthorized
-    /// pausing or unpausing, which can disrupt contract functionality. Remove
-    /// or secure these functions before deployment.
-    fn pause(&mut self) -> Result<(), Error> {
-        self.pausable.pause().map_err(|e| e.into())
-    }
-
-    fn unpause(&mut self) -> Result<(), Error> {
-        self.pausable.unpause().map_err(|e| e.into())
+    ) -> Result<(), erc20::Error> {
+        // ...
+        self.erc20.burn_from(account, value)
     }
 }
