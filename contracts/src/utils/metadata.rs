183c123bd972408bc66991ee8f31168a21e4d17b
murluki <timofeli.work@gmail.com>
2025-03-18 19:51:54 +0100

    fixed metadata in utils

--- contracts/src/utils/metadata.rs
+++ contracts/src/utils/metadata.rs
@@ -3,10 +3,6 @@ use alloc::{string::String, vec, vec::Vec};
 
 use stylus_sdk::{prelude::*, storage::StorageString};
 
-use crate::utils::introspection::erc165::{Erc165, IErc165};
-
-use alloy_primitives::FixedBytes;
-
 /// State of a [`Metadata`] contract.
 #[storage]
 pub struct Metadata {
@@ -34,32 +30,5 @@ impl Metadata {
     /// * `&self` - Read access to the contract's state.
     pub fn symbol(&self) -> String {
         self.symbol.get_string()
-    }
-}
-
-impl IErc165 for Metadata {
-    fn supports_interface(interface_id: FixedBytes<4>) -> bool {
-        Erc165::supports_interface(interface_id)
-    }
-}
-
-#[cfg(all(test, feature = "std"))]
-mod tests {
-    use super::*;
-    use alloy_primitives::Address;
-    use motsu::prelude::Contract;
-    use crate::utils::introspection::erc165::IErc165;
-
-    
-    const INTERFACE_ID_ERC165: u32 = 0x01ffc9a7;
-
-    #[motsu::test]
-    fn supports_interface() {
-        assert!(Metadata::supports_interface(
-            INTERFACE_ID_ERC165.into()
-        ));
-        
-        let fake_interface_id = 0x12345678u32;
-        assert!(!Metadata::supports_interface(fake_interface_id.into()));
     }
 }
