92f52945549f766b7b13972d96238f55fa686ff3
0xNeshi <nenad.misic@openzeppelin.com>
2025-03-26 10:10:29 +0100

    build: use newer nitro devnode

--- scripts/nitro-devnode.sh
+++ scripts/nitro-devnode.sh
@@ -5,31 +5,30 @@ cd "$MYDIR" || exit
 
 HAS_INIT=false
 
-while [[ $# -gt 0 ]]
-do
+while [[ $# -gt 0 ]]; do
   case "$1" in
-    -i|--init)
-      HAS_INIT=true
-      shift
-      ;;
-    -q|--quit)
-      NITRO_CONTAINERS=$(docker container ls -q --filter name=nitro)
+  -i | --init)
+    HAS_INIT=true
+    shift
+    ;;
+  -q | --quit)
+    NITRO_CONTAINERS=$(docker container ls -q --filter name=nitro)
 
-      if [ -z "$NITRO_CONTAINERS" ]; then
-          echo "No nitro node containers running"
-      else
-          docker container stop $NITRO_CONTAINERS || exit
-      fi
+    if [ -z "$NITRO_CONTAINERS" ]; then
+      echo "No nitro node containers running"
+    else
+      docker container stop $NITRO_CONTAINERS || exit
+    fi
 
-      exit 0
-      ;;
-    *)
-      echo "OPTIONS:"
-      echo "-i|--init:         clone repo and init nitro node"
-      echo "-q|--quit:         shutdown nitro node docker containers"
-      echo "-s|--stylus:       setup nitro node with Stylus dev dependencies"
-      exit 0
-      ;;
+    exit 0
+    ;;
+  *)
+    echo "OPTIONS:"
+    echo "-i|--init:         clone repo and init nitro node"
+    echo "-q|--quit:         shutdown nitro node docker containers"
+    echo "-s|--stylus:       setup nitro node with Stylus dev dependencies"
+    exit 0
+    ;;
   esac
 done
 
@@ -38,18 +37,16 @@ if [ ! -d "$DEV_NODE_DIR" ]; then
   HAS_INIT=true
 fi
 
-if $HAS_INIT
-then
+if $HAS_INIT; then
   cd "$MYDIR" || exit
   cd ..
 
   git clone --recurse-submodules https://github.com/OffchainLabs/nitro-devnode.git
   cd ./nitro-devnode || exit
   git pull origin release --recurse-submodules
-  git checkout 6cad5efa9c1d57ed9479c66701532f62e14d06c9 || exit
+  git checkout 799db550e35cbea02d923befdecf2d1b818cc58e || exit
 
 fi
-
 
 cd "$DEV_NODE_DIR" || exit
 ./run-dev-node.sh
