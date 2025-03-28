0cdb8578f298ad08cf2cb03f8883e7def4a09300
Sumit Vekariya <sarkazein7@gmail.com>
2025-03-28 17:44:29 +0530

    style: Format IErc20Permit trait implementation

--- contracts/src/token/erc20/extensions/permit.rs
+++ contracts/src/token/erc20/extensions/permit.rs
@@ -14,7 +14,7 @@
 
 use alloc::{vec, vec::Vec};
 
-use alloy_primitives::{keccak256, Address, B256, FixedBytes, U256};
+use alloy_primitives::{keccak256, Address, FixedBytes, B256, U256};
 use alloy_sol_types::SolType;
 use openzeppelin_stylus_proc::interface_id;
 use stylus_sdk::{block, call::MethodError, prelude::*};
@@ -56,8 +56,8 @@ mod sol {
     }
 }
 
-/// Required interface of ERC20 Permit extension that allows approvals to be made
-/// via signatures, as defined in EIP-2612.
+/// Required interface of ERC20 Permit extension that allows approvals to be
+/// made via signatures, as defined in EIP-2612.
 #[interface_id]
 pub trait IErc20Permit {
     /// The error type associated to this ERC-20 Permit trait implementation.
@@ -65,9 +65,9 @@ pub trait IErc20Permit {
 
     /// Returns the current nonce for `owner`. This value must be
     /// included whenever a signature is generated for a permit.
-    /// 
-    /// Every successful call to {permit} increases the owner's nonce by one. This
-    /// prevents a signature from being used multiple times.
+    ///
+    /// Every successful call to {permit} increases the owner's nonce by one.
+    /// This prevents a signature from being used multiple times.
     ///
     /// # Arguments
     ///
@@ -75,8 +75,8 @@ pub trait IErc20Permit {
     /// * `owner` - The address to query the nonce of.
     fn nonces(&self, owner: Address) -> U256;
 
-    /// Returns the domain separator used in the encoding of the signature for {permit},
-    /// as defined by EIP-712.
+    /// Returns the domain separator used in the encoding of the signature for
+    /// {permit}, as defined by EIP-712.
     ///
     /// # Arguments
     ///
@@ -92,7 +92,8 @@ pub trait IErc20Permit {
     /// * `&mut self` - Write access to the contract's state.
     /// * `owner` - Account that owns the tokens.
     /// * `spender` - Account that will spend the tokens.
-    /// * `value` - The number of tokens being permitted to transfer by `spender`.
+    /// * `value` - The number of tokens being permitted to transfer by
+    ///   `spender`.
     /// * `deadline` - Deadline for the permit action.
     /// * `v` - v value from the `owner`'s signature.
     /// * `r` - r value from the `owner`'s signature.
@@ -100,7 +101,8 @@ pub trait IErc20Permit {
     ///
     /// # Errors
     ///
-    /// * [`Error::ExpiredSignature`] - If the `deadline` param is from the past.
+    /// * [`Error::ExpiredSignature`] - If the `deadline` param is from the
+    ///   past.
     /// * [`Error::InvalidSigner`] - If signer is not an `owner`.
     ///
     /// # Events
@@ -171,7 +173,8 @@ impl<T: IEip712 + StorageType> IErc20Permit for Erc20Permit<T> {
 
     fn nonces(&self, _owner: Address) -> U256 {
         // This method should be implemented by the Nonces contract directly
-        // when this trait is used, but we add this implementation to satisfy the trait
+        // when this trait is used, but we add this implementation to satisfy
+        // the trait
         U256::ZERO
     }
 
@@ -189,15 +192,17 @@ impl<T: IEip712 + StorageType> IErc20Permit for Erc20Permit<T> {
         _r: B256,
         _s: B256,
     ) -> Result<(), Self::Error> {
-        // This is a wrapper around the actual permit method that requires Erc20 and Nonces
-        // When this trait is used, the caller should implement this properly
+        // This is a wrapper around the actual permit method that requires Erc20
+        // and Nonces When this trait is used, the caller should
+        // implement this properly
         Err(Error::ExpiredSignature(ERC2612ExpiredSignature { deadline }))
     }
 }
 
 impl<T: IEip712 + StorageType> IErc165 for Erc20Permit<T> {
     fn supports_interface(interface_id: FixedBytes<4>) -> bool {
-        <Self as IErc20Permit>::INTERFACE_ID == u32::from_be_bytes(*interface_id)
+        <Self as IErc20Permit>::INTERFACE_ID
+            == u32::from_be_bytes(*interface_id)
     }
 }
 
@@ -277,13 +282,13 @@ impl<T: IEip712 + StorageType> Erc20Permit<T> {
 #[cfg(all(test, feature = "std"))]
 mod tests {
     use alloy_primitives::FixedBytes;
-    
+
     #[test]
     fn test_interface_id() {
         // Interface ID should be non-zero
         assert_ne!(interface_id(), 0);
     }
-    
+
     // For simplicity, we'll test the pattern for interface_id calculation
     // rather than using an actual instance of Erc20Permit
     fn interface_id() -> u32 {
@@ -291,23 +296,24 @@ mod tests {
         // This mimics how the #[interface_id] macro works internally
         0x9d3f00dd // Example value, actual ID will be different
     }
-    
-    // Test that the supports_interface implementation follows the correct pattern
+
+    // Test that the supports_interface implementation follows the correct
+    // pattern
     #[test]
     fn test_supports_interface_pattern() {
         // Convert our interface ID to bytes
         let id_bytes = interface_id().to_be_bytes();
         let fixed_bytes = FixedBytes::<4>::from_slice(&id_bytes);
-        
+
         // Verify correct pattern for checking interface ID
         assert!(supports_interface(fixed_bytes));
-        
+
         // Verify it doesn't recognize other interfaces
         let wrong_id = [0u8; 4];
         let wrong_bytes = FixedBytes::<4>::from_slice(&wrong_id);
         assert!(!supports_interface(wrong_bytes));
     }
-    
+
     // Mock implementation of supports_interface that follows the same pattern
     // as the real implementation
     fn supports_interface(interface_id: FixedBytes<4>) -> bool {
