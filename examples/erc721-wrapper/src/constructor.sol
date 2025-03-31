3181b78e24d8b39838ec86ef56967dc92fbec4db
Daniel Bigos <daniel.bigos@icloud.com>
2025-04-01 01:59:49 +0200

    test: add E2E tests for Erc721Wrapper

--- examples/erc721-wrapper/src/constructor.sol
+++ examples/erc721-wrapper/src/constructor.sol
@@ -2,10 +2,11 @@
 pragma solidity ^0.8.21;
 
 contract Erc721WrapperExample {
-    mapping(address account => uint256) private _balances;
-    mapping(address account => mapping(address spender => uint256))
-        private _allowances;
-    uint256 private _totalSupply;
+    mapping(uint256 tokenId => address) private _owners;
+    mapping(address owner => uint256) private _balances;
+    mapping(uint256 tokenId => address) private _tokenApprovals;
+    mapping(address owner => mapping(address operator => bool))
+        private _operatorApprovals;
 
     address private _underlyingToken;
 
--- examples/erc721-wrapper/tests/abi/mod.rs
+++ examples/erc721-wrapper/tests/abi/mod.rs
@@ -4,36 +4,31 @@ use alloy::sol;
 sol!(
     #[sol(rpc)]
     contract Erc721Wrapper {
-        function name() external view returns (string name);
-        function symbol() external view returns (string symbol);
-        function totalSupply() external view returns (uint256 totalSupply);
-        function balanceOf(address account) external view returns (uint256 balance);
-        function transfer(address recipient, uint256 amount) external returns (bool);
-        function allowance(address owner, address spender) external view returns (uint256 allowance);
-        function approve(address spender, uint256 amount) external returns (bool);
-        function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
-
         #[derive(Debug)]
-        function decimals() external view returns (uint8 decimals);
+        function balanceOf(address owner) external view returns (uint256 balance);
         #[derive(Debug)]
         function underlying() external view returns (address underlying);
         #[derive(Debug)]
-        function depositFor(address account, uint256 value) external  returns (bool);
+        function ownerOf(uint256 tokenId) external view returns (address owner);
         #[derive(Debug)]
-        function withdrawTo(address account, uint256 value) external  returns (bool);
+        function depositFor(address account, uint256[] memory tokenIds) external returns (bool);
+        #[derive(Debug)]
+        function withdrawTo(address account, uint256[] memory tokenIds) external returns (bool);
 
-        error ERC721InvalidOwner(address owner);
+
+        #[derive(Debug, PartialEq)]
+        event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+
         error ERC721NonexistentToken(uint256 tokenId);
-        error ERC721IncorrectOwner(address sender, uint256 tokenId, address owner);
-        error ERC721InvalidSender(address sender);
-        error ERC721InvalidReceiver(address receiver);
-        error ERC721InsufficientApproval(address operator, uint256 tokenId);
-        error ERC721InvalidApprover(address approver);
-        error ERC721InvalidOperator(address operator);
+    }
+
+    contract Erc721 {
+        function safeTransferFrom(address from, address to, uint256 tokenId, bytes calldata data) external;
+
+        #[derive(Debug)]
+        function ownerOf(uint256 tokenId) external view returns (address owner);
 
         #[derive(Debug, PartialEq)]
-        event Transfer(address indexed from, address indexed to, uint256 value);
-        #[derive(Debug, PartialEq)]
-        event Approval(address indexed owner, address indexed spender, uint256 value);
+        event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
     }
 );
