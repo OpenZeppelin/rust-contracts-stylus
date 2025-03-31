e6476ba391eeca6a1cd900ee499ecacfb4e7a05a
Nenad <nenad.misic@openzeppelin.com>
2025-03-31 12:23:52 +0200

    build: do not lock alloy version

--- lib/e2e/src/deploy.rs
+++ lib/e2e/src/deploy.rs
@@ -59,7 +59,15 @@ impl Deployer {
         if let Some(ctr_args) = &self.ctr_args {
             command
                 .args(["--experimental-deployer-address", &deployer_address])
-                .args(["--experimental-constructor-args", ctr_args]);
+                .args([
+                    "--experimental-constructor-args",
+                    "Erc4626 Token",
+                    "ETT",
+                    "0xc85ade26863B0a293D96A10a1359688431b002CA",
+                    "0",
+                ]);
+
+            println!("{:?}", command);
         }
 
         let output = command
