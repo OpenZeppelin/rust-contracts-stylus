99ca48c6cbcf53913fa921cdc4b513ea7fada06d
Angry Coding <mailidanielifechukwu@gmail.com>
2025-04-09 23:06:11 +0000

    chore: Added Address

--- contracts/src/token/erc20/utils/safe_erc20.rs
+++ contracts/src/token/erc20/utils/safe_erc20.rs
@@ -401,8 +401,10 @@ impl IErc165 for SafeErc20 {
 
 #[cfg(all(test, feature = "std"))]
 mod tests {
+    use alloy_primitives::{Address, FixedBytes, U256};
     use super::{ISafeErc20, SafeErc20};
     use crate::utils::introspection::erc165::IErc165;
+
 
     #[test]
     fn encodes_true_empty_slice() {
@@ -435,19 +437,29 @@ mod tests {
     }
 
     #[motsu::test]
-    fn safe_transfer(alice: Address, bob: Address) {}
+    fn safe_transfer(alice: Address, bob: Address) {
+        assert!(true, true);
+    }
 
     #[motsu::test]
-    fn safe_transfer_from(alice: Address, bob: Address) {}
+    fn safe_transfer_from(alice: Address, bob: Address) {
+        assert!(true, true);
+    }
 
     #[motsu::test]
-    fn safe_increase_allowance(alice: Address, bob: Address) {}
+    fn safe_increase_allowance(alice: Address, bob: Address) {
+        assert!(true, true);
+    }
 
     #[motsu::test]
-    fn safe_decrease_allowance(alice: Address, bob: Address) {}
-    
-    #[motsu::test]
-    fn force_approve(alice: Address, bob: Address) {}
+    fn safe_decrease_allowance(alice: Address, bob: Address) {
+        assert!(true, true);
+    }
+
+    #[motsu::test] 
+    fn force_approve(alice: Address, bob: Address) {
+        assert!(true, true);
+    }
 
     #[motsu::test]
     fn interface_id() {
