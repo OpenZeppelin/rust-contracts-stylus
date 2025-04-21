49e1da55c02a8ba9f96a39742e1af8ff0ac439c5
0xNeshi <nenad.misic@openzeppelin.com>
2025-04-21 11:14:27 +0200

    test: add back single_verif fuzzing

--- fuzz/fuzz_targets/merkle.rs
+++ fuzz/fuzz_targets/merkle.rs
@@ -7,5 +7,19 @@ use test_fuzz::Input;
 fuzz_target!(|input: Input| {
     let Input { root, leaves, proof, proof_flags } = input;
 
-    _ = Verifier::verify_multi_proof(&proof, &proof_flags, root, &leaves);
+    let multi_verif =
+        Verifier::verify_multi_proof(&proof, &proof_flags, root, &leaves);
+
+    // If we have a single leaf, also test the regular verification
+    if leaves.len() == 1 {
+        let single_verif = Verifier::verify(&proof, root, leaves[0]);
+
+        if let Ok(multi_verif) = multi_verif {
+            // ensure the results match if no errors occurred
+            assert_eq!(single_verif, multi_verif);
+        } else {
+            // otherwise single-proof verification must be false
+            assert!(!single_verif);
+        }
+    }
 });
