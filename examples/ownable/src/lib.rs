3fd0c4111e6c598bcd191eeec0570b8747c36e8e
0xNeshi <nenad.misic@openzeppelin.com>
2025-05-08 09:27:19 +0200

    test: use new inherit model in ownable e2e

--- examples/ownable/src/lib.rs
+++ examples/ownable/src/lib.rs
@@ -5,7 +5,7 @@ use alloc::vec::Vec;
 
 use alloy_primitives::{Address, U256};
 use openzeppelin_stylus::{
-    access::ownable::{self, Ownable},
+    access::ownable::{self, IOwnable, Ownable},
     token::erc20::{self, Erc20, IErc20},
 };
 use stylus_sdk::prelude::*;
@@ -53,23 +53,78 @@ impl From<ownable::Error> for Error {
 #[entrypoint]
 #[storage]
 struct OwnableExample {
-    #[borrow]
     erc20: Erc20,
-    #[borrow]
     ownable: Ownable,
 }
 
 #[public]
-#[inherit(Erc20, Ownable)]
+#[implements(IErc20<Error = Error>, IOwnable<Error = Error>)]
 impl OwnableExample {
     #[constructor]
     fn constructor(&mut self, initial_owner: Address) -> Result<(), Error> {
         Ok(self.ownable.constructor(initial_owner)?)
     }
+}
 
-    fn transfer(&mut self, to: Address, value: U256) -> Result<(), Error> {
+#[public]
+impl IErc20 for OwnableExample {
+    type Error = Error;
+
+    fn total_supply(&self) -> U256 {
+        self.erc20.total_supply()
+    }
+
+    fn balance_of(&self, account: Address) -> U256 {
+        self.erc20.balance_of(account)
+    }
+
+    fn transfer(
+        &mut self,
+        to: Address,
+        value: U256,
+    ) -> Result<bool, <Self as IErc20>::Error> {
         self.ownable.only_owner()?;
-        self.erc20.transfer(to, value)?;
-        Ok(())
+        Ok(self.erc20.transfer(to, value)?)
+    }
+
+    fn allowance(&self, owner: Address, spender: Address) -> U256 {
+        self.erc20.allowance(owner, spender)
+    }
+
+    fn approve(
+        &mut self,
+        spender: Address,
+        value: U256,
+    ) -> Result<bool, <Self as IErc20>::Error> {
+        Ok(self.erc20.approve(spender, value)?)
+    }
+
+    fn transfer_from(
+        &mut self,
+        from: Address,
+        to: Address,
+        value: U256,
+    ) -> Result<bool, <Self as IErc20>::Error> {
+        Ok(self.erc20.transfer_from(from, to, value)?)
+    }
+}
+
+#[public]
+impl IOwnable for OwnableExample {
+    type Error = Error;
+
+    fn owner(&self) -> Address {
+        self.ownable.owner()
+    }
+
+    fn transfer_ownership(
+        &mut self,
+        new_owner: Address,
+    ) -> Result<(), <Self as IOwnable>::Error> {
+        Ok(self.ownable.transfer_ownership(new_owner)?)
+    }
+
+    fn renounce_ownership(&mut self) -> Result<(), <Self as IOwnable>::Error> {
+        Ok(self.ownable.renounce_ownership()?)
     }
 }
