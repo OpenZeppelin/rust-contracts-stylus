a33c977beed3a3c567ec72b5c0d2a58527e6ee27
0xNeshi <nenad.misic@openzeppelin.com>
2025-05-09 10:44:10 +0200

    test: fix access-control

--- examples/access-control/src/lib.rs
+++ examples/access-control/src/lib.rs
@@ -53,9 +53,7 @@ impl From<erc20::Error> for Error {
 #[entrypoint]
 #[storage]
 struct AccessControlExample {
-    #[borrow]
     erc20: Erc20,
-    #[borrow]
     access: AccessControl,
 }
 
@@ -63,7 +61,7 @@ pub const TRANSFER_ROLE: [u8; 32] =
     keccak_const::Keccak256::new().update(b"TRANSFER_ROLE").finalize();
 
 #[public]
-#[inherit(Erc20, AccessControl)]
+#[implements(IErc20<Error = Error>, IAccessControl<Error = Error>)]
 impl AccessControlExample {
     #[constructor]
     fn constructor(&mut self, admin: Address) {
@@ -77,20 +75,97 @@ impl AccessControlExample {
         Ok(())
     }
 
+    // WARNING: This should not be part of the public API, it's here for testing
+    // purposes only.
+    fn set_role_admin(&mut self, role: B256, new_admin_role: B256) {
+        self.access._set_role_admin(role, new_admin_role)
+    }
+}
+
+#[public]
+impl IErc20 for AccessControlExample {
+    type Error = Error;
+
+    fn total_supply(&self) -> U256 {
+        self.erc20.total_supply()
+    }
+
+    fn balance_of(&self, account: Address) -> U256 {
+        self.erc20.balance_of(account)
+    }
+
+    fn transfer(
+        &mut self,
+        to: Address,
+        value: U256,
+    ) -> Result<bool, <Self as IErc20>::Error> {
+        Ok(self.erc20.transfer(to, value)?)
+    }
+
+    fn allowance(&self, owner: Address, spender: Address) -> U256 {
+        self.erc20.allowance(owner, spender)
+    }
+
+    fn approve(
+        &mut self,
+        spender: Address,
+        value: U256,
+    ) -> Result<bool, <Self as IErc20>::Error> {
+        Ok(self.erc20.approve(spender, value)?)
+    }
+
     fn transfer_from(
         &mut self,
         from: Address,
         to: Address,
         value: U256,
-    ) -> Result<bool, Error> {
+    ) -> Result<bool, <Self as IErc20>::Error> {
         self.access.only_role(TRANSFER_ROLE.into())?;
         let transfer_result = self.erc20.transfer_from(from, to, value)?;
         Ok(transfer_result)
     }
+}
 
-    // WARNING: This should not be part of the public API, it's here for testing
-    // purposes only.
-    fn set_role_admin(&mut self, role: B256, new_admin_role: B256) {
-        self.access._set_role_admin(role, new_admin_role)
+#[public]
+impl IAccessControl for AccessControlExample {
+    type Error = Error;
+
+    fn has_role(&self, role: B256, account: Address) -> bool {
+        self.access.has_role(role, account)
+    }
+
+    fn only_role(
+        &self,
+        role: B256,
+    ) -> Result<(), <Self as IAccessControl>::Error> {
+        Ok(self.access.only_role(role)?)
+    }
+
+    fn get_role_admin(&self, role: B256) -> B256 {
+        self.access.get_role_admin(role)
+    }
+
+    fn grant_role(
+        &mut self,
+        role: B256,
+        account: Address,
+    ) -> Result<(), <Self as IAccessControl>::Error> {
+        Ok(self.access.grant_role(role, account)?)
+    }
+
+    fn revoke_role(
+        &mut self,
+        role: B256,
+        account: Address,
+    ) -> Result<(), <Self as IAccessControl>::Error> {
+        Ok(self.access.revoke_role(role, account)?)
+    }
+
+    fn renounce_role(
+        &mut self,
+        role: B256,
+        confirmation: Address,
+    ) -> Result<(), <Self as IAccessControl>::Error> {
+        Ok(self.access.renounce_role(role, confirmation)?)
     }
 }
