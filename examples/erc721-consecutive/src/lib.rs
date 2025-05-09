7e6683332b780303ce999b2511e13a20e83f4761
Daniel Bigos <daniel.bigos@icloud.com>
2025-05-09 12:33:03 +0200

    chore: fix erc721-consecutive example

--- examples/erc721-consecutive/src/lib.rs
+++ examples/erc721-consecutive/src/lib.rs
@@ -1,16 +1,15 @@
 #![cfg_attr(not(any(test, feature = "export-abi")), no_main)]
 extern crate alloc;
 
-use alloy_primitives::{aliases::U96, Address, FixedBytes, U256};
+use alloy_primitives::{aliases::U96, Address, /* FixedBytes, */ U256};
 use openzeppelin_stylus::{
     token::erc721::{
-        extensions::{consecutive, Erc721Consecutive},
-        Erc721,
+        extensions::{consecutive, Erc721Consecutive, IErc721Burnable},
+        IErc721,
     },
-    utils::introspection::erc165::IErc165,
+    // utils::introspection::erc165::IErc165,
 };
-use stylus_sdk::prelude::*;
-
+use stylus_sdk::{abi::Bytes, prelude::*};
 #[entrypoint]
 #[storage]
 struct Erc721ConsecutiveExample {
@@ -19,7 +18,7 @@ struct Erc721ConsecutiveExample {
 }
 
 #[public]
-#[inherit(Erc721Consecutive)]
+#[implements(IErc721<Error=consecutive::Error>, IErc721Burnable<Error=consecutive::Error>, /*IErc165*/)]
 impl Erc721ConsecutiveExample {
     #[constructor]
     fn constructor(
@@ -37,10 +36,6 @@ impl Erc721ConsecutiveExample {
         Ok(())
     }
 
-    fn burn(&mut self, token_id: U256) -> Result<(), consecutive::Error> {
-        self.erc721_consecutive._burn(token_id)
-    }
-
     fn mint(
         &mut self,
         to: Address,
@@ -48,8 +43,91 @@ impl Erc721ConsecutiveExample {
     ) -> Result<(), consecutive::Error> {
         self.erc721_consecutive._mint(to, token_id)
     }
+}
 
+#[public]
+impl IErc721 for Erc721ConsecutiveExample {
+    type Error = consecutive::Error;
+
+    fn balance_of(&self, owner: Address) -> Result<U256, consecutive::Error> {
+        Ok(self.erc721_consecutive.balance_of(owner)?)
+    }
+
+    fn owner_of(&self, token_id: U256) -> Result<Address, consecutive::Error> {
+        Ok(self.erc721_consecutive.owner_of(token_id)?)
+    }
+
+    fn safe_transfer_from(
+        &mut self,
+        from: Address,
+        to: Address,
+        token_id: U256,
+    ) -> Result<(), consecutive::Error> {
+        Ok(self.erc721_consecutive.safe_transfer_from(from, to, token_id)?)
+    }
+
+    fn safe_transfer_from_with_data(
+        &mut self,
+        from: Address,
+        to: Address,
+        token_id: U256,
+        data: Bytes,
+    ) -> Result<(), consecutive::Error> {
+        Ok(self
+            .erc721_consecutive
+            .safe_transfer_from_with_data(from, to, token_id, data)?)
+    }
+
+    fn transfer_from(
+        &mut self,
+        from: Address,
+        to: Address,
+        token_id: U256,
+    ) -> Result<(), consecutive::Error> {
+        Ok(self.erc721_consecutive.transfer_from(from, to, token_id)?)
+    }
+
+    fn approve(
+        &mut self,
+        to: Address,
+        token_id: U256,
+    ) -> Result<(), consecutive::Error> {
+        Ok(self.erc721_consecutive.approve(to, token_id)?)
+    }
+
+    fn set_approval_for_all(
+        &mut self,
+        to: Address,
+        approved: bool,
+    ) -> Result<(), consecutive::Error> {
+        Ok(self.erc721_consecutive.set_approval_for_all(to, approved)?)
+    }
+
+    fn get_approved(
+        &self,
+        token_id: U256,
+    ) -> Result<Address, consecutive::Error> {
+        Ok(self.erc721_consecutive.get_approved(token_id)?)
+    }
+
+    fn is_approved_for_all(&self, owner: Address, operator: Address) -> bool {
+        self.erc721_consecutive.is_approved_for_all(owner, operator)
+    }
+}
+
+#[public]
+impl IErc721Burnable for Erc721ConsecutiveExample {
+    type Error = consecutive::Error;
+
+    fn burn(&mut self, token_id: U256) -> Result<(), consecutive::Error> {
+        self.erc721_consecutive._burn(token_id)
+    }
+}
+
+/*
+#[public]
+impl IErc165 for Erc721ConsecutiveExample {
     fn supports_interface(interface_id: FixedBytes<4>) -> bool {
         Erc721::supports_interface(interface_id)
     }
-}
+}*/
