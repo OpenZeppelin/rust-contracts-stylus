84d613fd71f8f1aed00f0879e145e84daf3a27b5
Daniel Bigos <daniel.bigos@icloud.com>
2025-05-27 13:00:22 +0200

    ref: refactor scripts

--- scripts/bench.sh
+++ scripts/bench.sh
@@ -24,7 +24,9 @@ get_example_crate_names() {
   find ./examples -maxdepth 2 -type f -name "Cargo.toml" | xargs grep 'name = ' | grep -oE '".*"' | tr -d "'\""
 }
 
-cargo build --release --target wasm32-unknown-unknown -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort
+cargo build --release --target wasm32-unknown-unknown \
+  -Z build-std=std,panic_abort \
+  -Z build-std-features=panic_immediate_abort
 
 # Optimize contract's wasm for gas usage.
 for CRATE_NAME in $(get_example_crate_names); do
