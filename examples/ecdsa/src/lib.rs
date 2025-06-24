9da43e0e508796c54145bc809fa925d702a8743d
0xNeshi <nenad.misic@openzeppelin.com>
2025-06-24 14:04:43 +0200

    test: update ecdsa

--- examples/ecdsa/src/lib.rs
+++ examples/ecdsa/src/lib.rs
@@ -4,7 +4,9 @@ extern crate alloc;
 use alloc::vec::Vec;
 
 use alloy_primitives::{Address, B256};
-use openzeppelin_stylus::utils::cryptography::ecdsa;
+use openzeppelin_stylus::utils::{
+    cryptography::ecdsa, precompiles::Precompiles,
+};
 use stylus_sdk::prelude::*;
 
 #[entrypoint]
@@ -20,6 +22,6 @@ impl ECDSAExample {
         r: B256,
         s: B256,
     ) -> Result<Address, ecdsa::Error> {
-        ecdsa::recover(self, hash, v, r, s)
+        self.ecrecover(hash, v, r, s)
     }
 }
