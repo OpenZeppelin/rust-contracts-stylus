0c5161d6684c00f58964521b7c3acf889e7f377d
0xNeshi <nenad.misic@openzeppelin.com>
2025-07-07 09:46:41 +0200

    test: verify events and errors

--- examples/proxy/tests/proxy.rs
+++ examples/proxy/tests/proxy.rs
@@ -67,7 +67,13 @@ async fn delegate(alice: Account, bob: Account) -> Result<()> {
     assert_eq!(total_supply, amount);
 
     // check that the balance can be transferred through the proxy
-    watch!(contract.transfer(bob.address(), amount))?;
+    let receipt = receipt!(contract.transfer(bob.address(), amount))?;
+
+    assert!(receipt.emits(ProxyExample::Transfer {
+        from: alice.address(),
+        to: bob.address(),
+        value: amount,
+    }));
 
     let balance = contract.balanceOf(alice.address()).call().await?.balance;
     assert_eq!(balance, U256::ZERO);
@@ -77,6 +83,28 @@ async fn delegate(alice: Account, bob: Account) -> Result<()> {
 
     let total_supply = contract.totalSupply().call().await?.totalSupply;
     assert_eq!(total_supply, amount);
+
+    Ok(())
+}
+
+#[e2e::test]
+async fn delegate_returns_error(alice: Account, bob: Account) -> Result<()> {
+    let implementation_addr = erc20::deploy(&alice.wallet).await?;
+    let contract_addr = alice
+        .as_deployer()
+        .with_constructor(ctr(implementation_addr))
+        .deploy()
+        .await?
+        .contract_address;
+    let contract = ProxyExample::new(contract_addr, &alice.wallet);
+
+    let err = send!(contract.transfer(bob.address(), U256::from(1000)))
+        .expect_err("should revert");
+    assert!(err.reverted_with(ProxyExample::ERC20InsufficientBalance {
+        sender: alice.address(),
+        balance: U256::ZERO,
+        needed: U256::from(1000),
+    }),);
 
     Ok(())
 }
