5a390d147d2eeadfc501fd11cf34ac1dd77d61e9
0xNeshi <nenad.misic@openzeppelin.com>
2025-07-14 17:46:59 +0200

    test: add beacon-example scaffold

---
+++ examples/beacon/src/lib.rs
@@ -0,0 +1,42 @@
+#![cfg_attr(not(any(test, feature = "export-abi")), no_main)]
+extern crate alloc;
+
+use alloc::vec::Vec;
+
+use openzeppelin_stylus::proxy::{beacon::proxy::BeaconProxy, erc1967, IProxy};
+use stylus_sdk::{
+    abi::Bytes, alloy_primitives::Address, prelude::*, ArbResult,
+};
+
+#[entrypoint]
+#[storage]
+struct BeaconProxyExample {
+    beacon_proxy: BeaconProxy,
+}
+
+#[public]
+impl BeaconProxyExample {
+    #[constructor]
+    pub fn constructor(
+        &mut self,
+        beacon: Address,
+        data: Bytes,
+    ) -> Result<(), erc1967::utils::Error> {
+        self.beacon_proxy.constructor(beacon, data)
+    }
+
+    fn implementation(&self) -> Result<Address, stylus_sdk::call::Error> {
+        IProxy::implementation(self)
+    }
+
+    #[fallback]
+    fn fallback(&mut self, calldata: &[u8]) -> ArbResult {
+        Ok(self.do_fallback(calldata)?)
+    }
+}
+
+impl IProxy for BeaconProxyExample {
+    fn implementation(&self) -> Result<Address, stylus_sdk::call::Error> {
+        self.beacon_proxy.implementation()
+    }
+}
