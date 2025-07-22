8e8c40fafffaf3815b52af89558e2d43b294a219
Daniel Bigos <daniel.bigos@icloud.com>
2025-07-22 14:45:46 +0200

    chore: remove Erc721ReceiverMock

--- contracts/src/token/erc721/utils/holder.rs
+++ contracts/src/token/erc721/utils/holder.rs
@@ -2,10 +2,12 @@
 use alloc::{vec, vec::Vec};
 
 use alloy_primitives::{Address, FixedBytes, U256};
-use stylus_sdk::{abi::Bytes, prelude::*};
+use stylus_sdk::{abi::Bytes, function_selector, prelude::*};
 
 use crate::token::erc721::receiver::{IErc721Receiver, RECEIVER_FN_SELECTOR};
 
+/// The expected value returned from [`IErc721Receiver::on_erc721_received`].
+const SELECTOR: FixedBytes<4> = FixedBytes::new(RECEIVER_FN_SELECTOR);
 /// Default implementation of the [`IErc721Receiver`] trait.
 #[storage]
 pub struct Erc721Holder;
@@ -26,7 +28,7 @@ impl IErc721Receiver for Erc721Holder {
         _token_id: U256,
         _data: Bytes,
     ) -> Result<FixedBytes<4>, Self::Error> {
-        Ok(RECEIVER_FN_SELECTOR.into())
+        Ok(SELECTOR)
     }
 }
 
@@ -50,7 +52,7 @@ mod tests {
                 U256::from(1),
                 vec![].into()
             ),
-            Ok(RECEIVER_FN_SELECTOR.into())
+            Ok(SELECTOR)
         );
     }
 }
