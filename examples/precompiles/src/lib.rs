af69cc85b6bcfb127aa58f87e88b9f370c4fc15d
0xNeshi <nenad.misic@openzeppelin.com>
2025-07-24 12:13:20 +0200

    test: ref b1024 conversion

--- examples/precompiles/src/lib.rs
+++ examples/precompiles/src/lib.rs
@@ -27,19 +27,16 @@ impl PrecompilesExample {
     }
 
     fn call_bls12_g1_add(&self, a: Bytes, b: Bytes) -> Result<Bytes, Vec<u8>> {
-        let result = self.bls12_g1_add(
-            B1024::try_from(a.as_slice()).map_err(|_| {
-                precompiles::Error::Bls12G1AddInvalidInput(
-                    precompiles::BLS12G1AddInvalidInput {},
-                )
-            })?,
-            B1024::try_from(b.as_slice()).map_err(|_| {
-                precompiles::Error::Bls12G1AddInvalidInput(
-                    precompiles::BLS12G1AddInvalidInput {},
-                )
-            })?,
-        )?;
+        let result = self.bls12_g1_add(to_b1024(a)?, to_b1024(b)?)?;
 
         Ok(result)
     }
+}
+
+fn to_b1024(a: Bytes) -> Result<B1024, precompiles::Error> {
+    B1024::try_from(a.as_slice()).map_err(|_| {
+        precompiles::Error::Bls12G1AddInvalidInput(
+            precompiles::BLS12G1AddInvalidInput {},
+        )
+    })
 }
