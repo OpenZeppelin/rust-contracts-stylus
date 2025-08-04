440a585dbfda0e80d6dbe5b49fac6ab3829722fb
Daniel Bigos <daniel.bigos@openzeppelin.com>
2025-08-04 14:10:28 +0200

    docs: apply CR comment
    Co-authored-by: Nenad <nenad.misic@openzeppelin.com>

--- contracts/src/proxy/utils/uups_upgradeable.rs
+++ contracts/src/proxy/utils/uups_upgradeable.rs
@@ -198,7 +198,9 @@ impl UUPSUpgradeable {
     /// Check that the execution is being performed through a
     /// [`stylus_sdk::call::delegate_call`] call and that the execution
     /// context is a proxy contract with an implementation (as defined in
-    /// ERC-1967) pointing to `self`. This should only be the case for
+    /// ERC-1967) pointing to `self`.
+    ///
+    /// This should only be the case for
     /// UUPS and transparent proxies that are using the current contract as
     /// their implementation. Execution of a function through ERC-1167
     /// minimal proxies (clones) would not normally pass this test, but is
