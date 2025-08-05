3e1e8aabaa0b62bb651137bd6119d57f362bae90
Daniel Bigos <daniel.bigos@icloud.com>
2025-08-06 01:51:18 +0200

    feat: add Erc721 Holder example
    (cherry picked from commit 515cc7adab8edc3bbb32a400b4119e3f520bdcaf)

---
+++ examples/erc721-holder/tests/erc721-holder.rs
@@ -0,0 +1,46 @@
+#![cfg(feature = "e2e")]
+
+use abi::Erc721HolderExample;
+use alloy::primitives::{Bytes, U256};
+use e2e::Account;
+use eyre::Result;
+use openzeppelin_stylus::token::erc721::RECEIVER_FN_SELECTOR;
+
+mod abi;
+
+#[e2e::test]
+async fn returns_correct_selector(alice: Account) -> Result<()> {
+    let contract_addr = alice.as_deployer().deploy().await?.contract_address;
+
+    let contract = Erc721HolderExample::new(contract_addr, &alice.wallet);
+
+    // call without data.
+    let interface_selector = contract
+        .onERC721Received(
+            alice.address(),
+            alice.address(),
+            U256::from(1),
+            Bytes::new(),
+        )
+        .call()
+        .await?
+        ._0;
+
+    assert_eq!(RECEIVER_FN_SELECTOR, interface_selector);
+
+    // call with data.
+    let interface_selector = contract
+        .onERC721Received(
+            alice.address(),
+            alice.address(),
+            U256::from(42),
+            Bytes::from(vec![0xde, 0xad, 0xbe, 0xef]),
+        )
+        .call()
+        .await?
+        ._0;
+
+    assert_eq!(RECEIVER_FN_SELECTOR, interface_selector);
+
+    Ok(())
+}
