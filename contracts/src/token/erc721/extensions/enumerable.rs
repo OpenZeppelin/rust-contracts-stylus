2fa4fcc34230a72a0367042b5c0d0b879d39b2f0
Alisander Qoshqosh <qalisander@gmail.com>
2025-08-18 11:56:25 +0400

    ++

--- contracts/src/token/erc721/extensions/enumerable.rs
+++ contracts/src/token/erc721/extensions/enumerable.rs
@@ -556,9 +556,7 @@ mod tests {
         assert!(Erc721Enumerable::_check_increase_balance(0).is_ok());
 
         let err = Erc721Enumerable::_check_increase_balance(1)
-            .motsu_expect_err(
-                "should return Error::EnumerableForbiddenBatchMint",
-            );
+            .expect_err("should return Error::EnumerableForbiddenBatchMint");
 
         assert!(matches!(
             err,
