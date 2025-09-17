ba3bd6d85308879659ba844e87a9d6491f41050e
Alisander Qoshqosh <qalisander@gmail.com>
2025-09-17 12:11:47 +0400

    migrate utils::metadata

--- contracts/src/utils/mod.rs
+++ contracts/src/utils/mod.rs
@@ -4,12 +4,12 @@
 // pub mod cryptography;
 pub mod introspection;
 pub mod math;
-// pub mod metadata;
+pub mod metadata;
 // pub mod nonces;
 // pub mod pausable;
 // pub mod precompiles;
 // pub mod storage_slot;
 pub mod structs;
 //
-// pub use metadata::Metadata;
+pub use metadata::Metadata;
 // pub use pausable::{IPausable, Pausable};
